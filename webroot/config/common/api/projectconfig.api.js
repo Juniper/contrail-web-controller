4a5a0627108b95b90c62aec2fdcb2e93f59e7c74
Balaji Kumar Ananthu <asbalaji@juniper.net>
2014-06-20 02:44:28 +0530

    Adding support for multiple domains in config pages - Networks, Policies, IPAM, Floating IP, Service Instance

--- webroot/config/common/api/projectconfig.api.js
+++ webroot/config/common/api/projectconfig.api.js
@@ -67,12 +67,31 @@ function listProjectsAPIServer (error, projectLists, response, appData)
         commonUtils.handleJSONResponse(error, response, null);
         return;
     }
-    configApiServer.apiGet(projectURL, appData,
+    /*configApiServer.apiGet(projectURL, appData,
                          function(error, data) {
                          listProjectsCb(error, data,
                                         projectLists, response)
                          });
-
+    */
+    if(projectLists && projectLists.hasOwnProperty("tenants")) {
+        var projects = {};
+        projects["projects"] = [];
+        var tenantLen = projectLists['tenants'].length;
+        for(var i=0; i<tenantLen; i++) {
+            var tenant = projectLists['tenants'][i];
+            projects["projects"].push({
+                "uuid"    : convertKeystoneUuidToAPIServerUUID(tenant["id"]),
+                "fq_name" : [
+                    "default-domain",
+                    tenant["name"]
+                ]
+            });
+        }
+        commonUtils.handleJSONResponse(error, response, projects);
+    } else {
+        commonUtils.handleJSONResponse(error, response, null);
+        return;
+    }
 }
 
 /**
@@ -153,5 +172,113 @@ function listDomains (request, response, appData)
                          });
 }
 
+/**
+ * @getProjectByParameterCb
+ * private function
+ * 1. Callback for getProjectByParameter
+ * 2. Reads the response of project get from config api server
+ *    and sends it back to the client.
+ */
+function getProjectByParameterCb(error, projectConfig, callback) 
+{
+    if (error) {
+        commonUtils.handleJSONResponse(error, response, null);
+        return;
+    }
+    callback(error, projectConfig);
+}
+
+/**
+ * @getProjectByParameter
+ * public function
+ * 1. URL /api/tenants/config/project/:id
+ * 2. Gets list of virtual networks from config api server
+ * 3. Needs tenant id
+ * 4. Calls getProjectByParameterCb that process data from config
+ *    api server and sends back the http response.
+ */
+function getProjectByParameter (request, response, appData) 
+{
+    var projectId = null;
+    var requestParams    = url.parse(request.url, true);
+    var params = {};
+    if(null !== request.param("exclude_children") && 
+        typeof request.param("exclude_children") !== "undefined")
+        params.exclude_children = request.param("exclude_children");
+    
+    if(null !== request.param("exclude_back_refs") && 
+        typeof request.param("exclude_back_refs") !== "undefined")
+        params.exclude_back_refs = request.param("exclude_back_refs");
+
+    if ((projectId = request.param('id'))) {
+        getProjectAsync({uuid:projectId, appData:appData, params: params},
+                       function(err, data) {
+            commonUtils.handleJSONResponse(err, response, data);
+        });
+    } else {
+        /**
+         * TODO - Add Language independent error code and return
+         */
+    }
+}
+
+function getProjectAsync (projectObj, callback)
+{
+    var projectId = projectObj['uuid'];
+    var appData   = projectObj['appData'];
+    var params    = projectObj['params'];
+
+    var reqUrl = '/project/' + projectId;
+    reqUrl = getUrlWithParameters(reqUrl, params);
+    configApiServer.apiGet(reqUrl, appData, function(err, data) {
+        getProjectByParameterCb(err, data, callback);
+    });
+}
+
+function getUrlWithParameters(baseURL, params) {
+    var reqUrl = baseURL;
+    if(!isEmpty(params)) {
+        reqUrl += "?";
+        if(params.hasOwnProperty("exclude_back_refs")) {
+            reqUrl += "exclude_back_refs=" + params["exclude_back_refs"];
+            delete params["exclude_back_refs"];
+        }
+        if(!isEmpty(params)) {
+            reqUrl += "&";
+            if(params.hasOwnProperty("exclude_children")) {
+                
+                reqUrl += "exclude_children=" + params["exclude_children"];
+                delete params["exclude_children"];
+            }
+        }
+    }
+    return reqUrl;
+}
+
+function isEmpty(obj) {
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+    return true;
+}
+/**
+* @convertKeystoneUuidToAPIServerUUID
+* Keystone UUID doesnt contain dashes '-'
+* API server UUID contains dashes in the 8-4-4-4-12 format
+* This function takes keystone UUID and converts to API Server UUID format.
+*/
+function convertKeystoneUuidToAPIServerUUID(keystone_uuid) {
+    var api_uuid_format = "";
+    api_uuid_format =
+        keystone_uuid.substr(0, 8) + '-' +
+        keystone_uuid.substr(8, 4) + '-' +
+        keystone_uuid.substr(12, 4) + '-' + 
+        keystone_uuid.substr(16, 4) + '-' + 
+        keystone_uuid.substr(20, 12);
+    return api_uuid_format;
+}
+
 exports.listDomains  = listDomains;
 exports.listProjects = listProjects;
+exports.getProjectByParameter   = getProjectByParameter;
