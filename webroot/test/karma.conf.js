d751bb114bca458b80782f2f00ed09a6fe606093
Naga Kiran <nagakiran@juniper.net>
2014-06-30 15:43:13 +0530

    1. Changes to share karma config settings across different targets (one target for each feature) Command to run test cases for a feature: $cd contrail-web-controller/webroot/test $grunt karma:monitor_infra  //It will run all the test cases for Monitor Infrastructure $grunt karma                //With no sub target,it runs the test cases across all features
    2. Changes to use sinon npm module instead of including "sinon.js" file

--- webroot/test/karma.conf.js
+++ webroot/test/karma.conf.js
@@ -5,50 +5,15 @@ module.exports = function(config) {
   config.set({
     basePath: '../../..',    //"contrail-web-ui" directory
     autoWatch: true,
-    frameworks: ['qunit'],
+    frameworks: ['qunit','sinon'],
     files: [
         //{pattern:"webroot/monitor/bgp/test/monitor_infra_dashboard.html",watched:false},
-        "contrail-web-core/webroot/js/contrail-all-1.js",
-        "contrail-web-core/webroot/js/contrail-all-2.js",
-        "contrail-web-core/webroot/js/contrail-all-3.js",
-        "contrail-web-core/webroot/js/contrail-all-4.js",
-        "contrail-web-core/webroot/js/contrail-all-5.js",
-        "contrail-web-core/webroot/assets/sinon/sinon.js",
-        "contrail-web-core/webroot/js/test/utils_mock.js",
-        "contrail-web-controller/webroot/monitor/infra/dashboard/ui/js/monitor_infra_dashboard.js",
-        "contrail-web-controller/webroot/monitor/infra/vrouter/ui/js/monitor_infra_vrouter.js",
-        "contrail-web-controller/webroot/monitor/infra/controlnode/ui/js/monitor_infra_controlnode.js",
-    	"contrail-web-controller/webroot/monitor/infra/analyticsnode/ui/js/monitor_infra_analyticnode.js",
-        "contrail-web-controller/webroot/monitor/infra/test/monitor_infra_dashboard_mock.js",
-        "contrail-web-controller/webroot/monitor/infra/test/monitor_infra_dashboard_test.js",
-        "contrail-web-controller/webroot/monitor/infra/test/monitor_infra_computenode_mock.js",
-        "contrail-web-controller/webroot/monitor/infra/test/monitor_infra_computenode_test.js",
-    	"contrail-web-controller/webroot/monitor/infra/test/monitor_infra_controlnode_mock.js",
-    	"contrail-web-controller/webroot/monitor/infra/test/monitor_infra_controlnode_test.js",
-    	"contrail-web-controller/webroot/monitor/infra/test/monitor_infra_analyticsnode_mock.js",
-    	"contrail-web-controller/webroot/monitor/infra/test/monitor_infra_analyticsnode_test.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/network/ui/js/tenant_monitor_network.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/dashboard/ui/js/tenant_monitor_dashboard.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/instance/ui/js/tenant_monitor_instance.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/project/ui/js/tenant_monitor_project.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/common/ui/js/tenant_monitor_topology.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/test/tenant_monitor_network_mock.js",
-        "contrail-web-controller/webroot/monitor/tenant-network/test/tenant_monitor_network_test.js",
-
-        "contrail-web-controller/webroot/config/services/instances/ui/js/svcinstances_config.js",
-        "contrail-web-controller/webroot/config/services/instances/ui/views/svcinstances_config.view",
-        "contrail-web-controller/webroot/config/services/instances/ui/views/svcinstances_config_ut.view",
-        "contrail-web-controller/webroot/config/services/instances/test/config_service_instance_test.js",
-        "contrail-web-controller/webroot/config/services/instances/test/config_service_instance_mock.js",
-        "contrail-web-controller/webroot/config/services/template/ui/js/svctemplate_config.js",
-        "contrail-web-controller/webroot/config/services/template/ui/views/svctemplate_config.view",
-        "contrail-web-controller/webroot/config/services/template/test/config_service_template_test.js",
-        "contrail-web-controller/webroot/config/services/template/test/config_service_template_mock.js",
     ],
     plugins:[
         'karma-phantomjs-launcher',
         'karma-coverage',
         'karma-qunit',
+        'karma-sinon',
         'karma-htmlfile-reporter',
         'karma-junit-reporter',
         'karma-html2js-preprocessor',
@@ -68,17 +33,11 @@ module.exports = function(config) {
       suite: ''
     },
     preprocessors: { 
-        'contrail-web-controller/webroot/monitor/bgp/js/*.js': ['coverage'],
-        'contrail-web-controller/webroot/monitor/tenant_network/js/*.js': ['coverage'],
-        'contrail-web-controller/webroot/config/services/instances/ui/js/*.js': ['coverage'],
-        'contrail-web-controller/webroot/config/services/instances/ui/views/*.view' : ['html2js'],
-        'contrail-web-controller/webroot/config/services/template/ui/js/*.js': ['coverage'],
-        'contrail-web-controller/webroot/config/services/template/ui/views/*.view' : ['html2js'],
         '*.html': []
         },
     htmlReporter: {
       outputFile: './tests/units.html'
     },
-    singleRun: false
+    singleRun: true
   });
 };
