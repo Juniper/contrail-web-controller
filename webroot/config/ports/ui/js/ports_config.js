72c9df4d1744732820163395597ba7de0e8ed31b
Naga Kiran <nagakiran@juniper.net>
2014-12-08 17:02:21 +0530

    Related-Bug: #1390392 1. Hide Configure > Networking > Routers menu in case of vCenter    orchestration 2. Hide Device Owner information in the following cases in Configure > Networking > Ports page    in case of vCenter    a. on row expansion    b. Owner column in grid    c. in Add/Edit popup
    Change-Id: I3338ccb4baee011cff4b0150ef6bf751ce05f0b7

--- webroot/config/ports/ui/js/ports_config.js
+++ webroot/config/ports/ui/js/ports_config.js
@@ -94,6 +94,15 @@ function fetchData() {
 }
 
 function initComponents() {
+    var columnsToBeAddedDynamically = [];
+    var ownerColumn = {
+                id: "devOwnerName",
+                field: "devOwnerName",
+                name: "Owner",
+                width:150
+            };
+    if(!isVCenter())
+        columnsToBeAddedDynamically.push(ownerColumn);
     $("#gridPorts").contrailGrid({
         header : {
             title : {
@@ -162,13 +171,7 @@ function initComponents() {
                     return returnString;
                 },
                 width:150
-            },
-            {
-                id: "devOwnerName",
-                field: "devOwnerName",
-                name: "Owner",
-                width:150
-            },
+            }
             /*{
                 id: "AllowedAddressPair",
                 field: "AllowedAddressPair",
@@ -179,7 +182,7 @@ function initComponents() {
                 field: "status",
                 name: "Status"
             }*/
-            ]
+            ].concat(columnsToBeAddedDynamically)
         },
         body : {
             options : {
@@ -1375,6 +1378,10 @@ function showPortEditWindow(mode, rowIndex) {
     if($("#btnCreatePorts").hasClass('disabled-link')) {
         return;
     }
+    if(!isVCenter()) {
+        $('#ddDeviceOwnerName').parents('.controls').parent().show();
+    } else
+        $('#ddDeviceOwnerName').parents('.controls').parent().hide();
     if (mode === "add") {
         windowCreatePorts.find('.modal-header-title').text('Create Port');
     } else {
@@ -1621,9 +1628,11 @@ function showPortEditWindow(mode, rowIndex) {
                     $("#ddVNState").data("contrailDropdown").value("false");
                 }
                 
-                $("#ddDeviceOwnerName").data("contrailDropdown").value(mapedData.deviceOwnerValue);
-                updateDevice();
-                $("#ddDeviceOwnerUUID").data("contrailDropdown").value(mapedData.deviceOwnerUUIDValue);
+                if(!isVCenter()) {
+                    $("#ddDeviceOwnerName").data("contrailDropdown").value(mapedData.deviceOwnerValue);
+                    updateDevice();
+                    $("#ddDeviceOwnerUUID").data("contrailDropdown").value(mapedData.deviceOwnerUUIDValue);
+                }
                 if(mapedData.sgMSValues.length > 0){
                     $("#is_SG")[0].checked = true;
                     $("#msSecurityGroup").data('contrailMultiselect').enable(true);
@@ -2388,6 +2397,7 @@ function destroy() {
         ddAAP.destroy();
         ddAAP = $();
     }*/
+    if(!isVCenter()) {
     ddDeviceOwnerName = $("#ddDeviceOwnerName").data("contrailDropdown");
     if(isSet(ddDeviceOwnerName)) {
         ddDeviceOwnerName.destroy();
@@ -2397,6 +2407,7 @@ function destroy() {
     if(isSet(ddDeviceOwnerUUID)) {
         ddDeviceOwnerUUID.destroy();
         ddDeviceOwnerUUID = $();
+    }
     }
 
     /*ddTenentID = $("#ddTenentID").data("contrailDropdown");
