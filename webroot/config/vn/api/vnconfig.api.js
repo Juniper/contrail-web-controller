1446c38af84af65ca9bc322517fcc85e41a68539
Biswajit Mandal <bmandal@juniper.net>
2015-02-03 01:17:14 +0530

    In stead of PUT for network_ipam_refs in seperate call, send it in same POST request to create VN
    Closes-Bug: #1417247
    
    Conflicts:
    	webroot/config/vn/api/vnconfig.api.js
    Change-Id: I376ab7a6e99531891c08afb851d70ae33415062c

--- webroot/config/vn/api/vnconfig.api.js
+++ webroot/config/vn/api/vnconfig.api.js
@@ -474,47 +474,6 @@ function createVirtualNetworkCb (error, vnConfig, vnPostData, response, appData)
 }
 
 /**
- * @createVNSubnetAdd
- * private function
- * 1. Callback for CreateVirtualNetwork
- */
-function createVNSubnetAdd (error, vnConfig, vnPostData,
-                            request, response, appData) 
-{
-    var subnetPostData   = {};
-    var netIpamRef       = null;
-    var subnet_prefix    = null;
-
-    if (error) {
-        commonUtils.handleJSONResponse(error, response, null);
-        return;
-    }
-
-    if ('network_ipam_refs' in vnPostData['virtual-network']) {
-        netIpamRef = vnPostData['virtual-network']['network_ipam_refs'];
-        if (!netIpamRef[0]['attr']['ipam_subnets'][0]
-                          ['subnet']['ip_prefix'].length) {
-            delete vnPostData['virtual-network']['network_ipam_refs'];
-        } else {
-            var reqUrl = '/virtual-network/' +
-                vnConfig['virtual-network']['uuid'].toString();
-            var putData = { 'virtual-network' :
-                {
-                    'fq_name': vnPostData['virtual-network']['fq_name'],
-                    'network_ipam_refs': netIpamRef
-                }
-            };
-            configApiServer.apiPut(reqUrl, putData, appData, function(error, data) {
-                createVirtualNetworkCb (error, vnConfig,
-                                        vnPostData, response, appData);
-            });
-            return;
-        }
-    }
-    createVirtualNetworkCb (error, vnConfig, vnPostData, response, appData);
-}
-
-/**
  * @setVNPolicySequence
  * private function
  * 1. Iterates through policy refs and sets the sequence numbers,
@@ -913,7 +872,6 @@ function createVirtualNetwork (request, response, appData)
     }
 
     vnConfigData = JSON.parse(JSON.stringify(vnPostData)); 
-    delete vnPostData['virtual-network']['network_ipam_refs'];
 
     if ('route_target_list' in vnPostData['virtual-network']) {
         if (!(vnPostData['virtual-network']['route_target_list']
@@ -924,11 +882,16 @@ function createVirtualNetwork (request, response, appData)
 
 
     vnSeqPostData = setVNPolicySequence(vnPostData);
+    if ('network_ipam_refs' in vnSeqPostData['virtual-network']) {
+        netIpamRef = vnSeqPostData['virtual-network']['network_ipam_refs'];
+        if (!netIpamRef[0]['attr']['ipam_subnets'][0]
+                          ['subnet']['ip_prefix'].length) {
+            delete vnSeqPostData['virtual-network']['network_ipam_refs'];
+        }
+    }
     configApiServer.apiPost(vnCreateURL, vnSeqPostData, appData,
                          function(error, data) {
-                         createVNSubnetAdd(error, data,
-                                           vnConfigData, request, response,
-                                           appData);
+        createVirtualNetworkCb(error, data, vnPostData, response, appData);
     });
 }
 
@@ -2202,4 +2165,3 @@ exports.updateVNRouteTargets         = updateVNRouteTargets;
 exports.getSharedVirtualNetworks     = getSharedVirtualNetworks;
 exports.getExternalVirtualNetworks   = getExternalVirtualNetworks;
 exports.getPagedVirtualNetworks      = getPagedVirtualNetworks;
-
