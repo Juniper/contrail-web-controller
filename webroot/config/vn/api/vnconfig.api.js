e7e5e0253e529d43ebcaa7d5ea69dc1c19f50f1b
Biswajit Mandal <bmandal@juniper.net>
2015-02-26 17:11:00 +0530

    Using the APIs to get the diff of VN Config while doing a PUT.
    Change-Id: I58995a11cffdd7b0d3a9eb75b9bbe3ddcae3ea97
    Related-Bug: #1423815

--- webroot/config/vn/api/vnconfig.api.js
+++ webroot/config/vn/api/vnconfig.api.js
@@ -25,6 +25,8 @@ var appErrors   = require(process.mainModule.exports["corePath"] +
                           '/src/serverroot/errors/app.errors');
 var util        = require('util');
 var url         = require('url');
+var jsonDiff    = require(process.mainModule.exports['corePath'] +
+                          '/src/serverroot/common/jsondiff');
 var configApiServer = require(process.mainModule.exports["corePath"] +
                               '/src/serverroot/common/configServer.api');
 
@@ -761,6 +763,11 @@ function updateFloatingIpList (vnId, vnPutData, appData, response, callback)
     });
 }
 
+var optFields = ['virtual_network_properties', 'access_control_lists',
+    'network_ipam_refs', 'network_policy_refs', 'routing_instances',
+    'virtual_network_properties', 'route_target_list', 'floating_ip_pools'];
+var mandateFields = ['fq_name', 'uuid'];
+
 function updateVNPolicyRefs (vnConfig, response, appData)
 {
     var vnPutData = response.req.body;
@@ -807,16 +814,9 @@ function updateVNPolicyRefs (vnConfig, response, appData)
     }
 
     vnPutData['virtual-network']['uuid'] = vnId;
-    if (null !=
-        vnSeqConfig['virtual-network']['virtual_machine_interface_back_refs']) {
-        delete
-            vnSeqConfig['virtual-network']['virtual_machine_interface_back_refs'];
-    }
-    if (null !=
-        vnSeqConfig['virtual-network']['instance_ip_back_refs']) {
-        delete vnSeqConfig['virtual-network']['instance_ip_back_refs'];
-    }
-    configApiServer.apiPut(vnPutURL, vnSeqConfig, appData, function(err, data) {
+    jsonDiff.getJSONDiffByConfigUrl(vnPutURL, appData, vnSeqConfig,
+                                    function(err, delta) {
+      configApiServer.apiPut(vnPutURL, delta, appData, function(err, data) {
         if (err) {
             commonUtils.handleJSONResponse(err, response, null);
             return;
@@ -825,7 +825,8 @@ function updateVNPolicyRefs (vnConfig, response, appData)
                                 function(err, data) {
             commonUtils.handleJSONResponse(err, response, data);
         });
-    }); 
+      });
+    });
 }
 
 function updateVirtualNetwork (request, response, appData)
