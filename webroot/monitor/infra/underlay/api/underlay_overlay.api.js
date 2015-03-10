970c63896196c76e9b021a9f88b330273c01718d
Biswajit Mandal <bmandal@juniper.net>
2015-03-10 15:02:03 +0530

    Adding flag in physical-topology to send if the topology has changed from last Adding vrouter in more_attr of VM in topology seen.
    Change-Id: Ie40132062c423e23e35174cf1129b41489db2494
    Related-Bug: #1427082

--- webroot/monitor/infra/underlay/api/underlay_overlay.api.js
+++ webroot/monitor/infra/underlay/api/underlay_overlay.api.js
@@ -20,6 +20,8 @@ var rest = require(process.mainModule.exports["corePath"] + '/src/serverroot/com
                         '/src/serverroot/common/opServer.api'),
   configApiServer = require(process.mainModule.exports["corePath"] +
                             '/src/serverroot/common/configServer.api'),
+  jsonDiff = require(process.mainModule.exports["corePath"] +
+                     '/src/serverroot/common/jsondiff'),
   queries = require(process.mainModule.exports["corePath"] +
                     '/src/serverroot/common/queries.api');
 
@@ -71,7 +73,9 @@ function buildvRouterVMTopology (nodeList, appData, callback)
                         ['interface_list'][0]['vm_name'],
                     'interface_list':
                         vmData['value'][i]['value']['UveVirtualMachineAgent']
-                               ['interface_list']};
+                               ['interface_list'],
+                    'vrouter':
+                        vmData['value'][i]['value']['UveVirtualMachineAgent']['vrouter']};
                 vmList.push({'name': vmData['value'][i]['name'],
                     'node_type': ctrlGlobal.NODE_TYPE_VIRTUAL_MACHINE,
                     'chassis_type': ctrlGlobal.NODE_TYPE_NONE,
@@ -118,6 +122,7 @@ function buildTopology (req, appData, callback)
     });
 }
 
+
 /* Function: getUnderlayTopology
  *  Get the Underlay Topology
  */
@@ -126,9 +131,32 @@ function getUnderlayTopology (req, res, appData)
 
     var url = '/analytics/uves/prouter';
     var key = global.STR_GET_UNDERLAY_TOPOLOGY + '@' + url;
+    var topologyChanged = true;
     redisUtils.checkAndGetRedisDataByKey(key, buildTopology, req, appData,
                                          function(err, topology) {
-        commonUtils.handleJSONResponse(err, res, topology); 
+        var forceRefresh = req.param('forceRefresh');
+        if (null != forceRefresh) {
+            /* Check if we have same topology or there is any change */
+            process.mainModule.exports.redisClient.get(key,
+                                                       function(error, value) {
+                if ((null != error) || (null == value)) {
+                    topology['topologyChanged'] = true;
+                    commonUtils.handleJSONResponse(err, res, topology);
+                    return;
+                }
+                value = JSON.parse(value);
+                var delta = jsonDiff.getConfigJSONDiff('physical-topology',
+                                                       value, topology);
+                if (null == delta) {
+                    topology['topologyChanged'] = false;
+                } else {
+                    topology['topologyChanged'] = true;
+                }
+                commonUtils.handleJSONResponse(err, res, topology);
+            });
+        } else {
+            commonUtils.handleJSONResponse(err, res, topology);
+        }
     });
 }
 
