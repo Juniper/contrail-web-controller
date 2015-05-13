8e42d75b905eed371cc9a9b7b8fca102206f72a2
Vishnu Vardhan V <vishnuvv@juniper.net>
2015-05-13 16:10:55 +0530

    Related-Bug:#1441351 In Underlay page,We are showing the traffic statistics tab on click of link between the virtual machine and vrouter
    Change-Id: Iafacad608ebd03dddaacf61c7056d2b95738e38e

--- webroot/common/ui/js/controller.utils.js
+++ webroot/common/ui/js/controller.utils.js
@@ -4,6 +4,7 @@
 
 define([
     'underscore',
+    'contrail-view-model',
     'monitor/networking/ui/js/views/NetworkingGraphView',
     'monitor/networking/ui/js/views/ProjectTabView',
     'monitor/networking/ui/js/views/NetworkTabView',
@@ -25,7 +26,7 @@ define([
     'monitor/alarms/ui/js/views/AlarmListView',
     'monitor/alarms/ui/js/views/AlarmGridView',
     'monitor/networking/ui/js/views/InterfaceGridView'
-], function (_, NetworkingGraphView, ProjectTabView, NetworkTabView, NetworkGridView, InstanceTabView, InstanceGridView,
+], function (_,ContrailViewModel, NetworkingGraphView, ProjectTabView, NetworkTabView, NetworkGridView, InstanceTabView, InstanceGridView,
              ProjectGridView, FlowGridView, NetworkListView, ProjectListView, InstanceListView, FlowListView, InstanceView,
              InstanceTrafficStatsView, ProjectView, NetworkView, ConnectedNetworkTabView, ConnectedNetworkTrafficStatsView,
              AlarmListView, AlarmGridView, InterfaceGridView) {
@@ -85,7 +86,214 @@ define([
                 }
             };
         };
+        self.getHeatChartClickFn = function(selector, response) {
+            // TODO: Implement click out function for instance port map
+            return function(clickData) {}
+        };
 
+        self.getInstanceTabViewConfig = function (viewConfig) {
+            var instanceUUID = viewConfig['instanceUUID'],
+                instanceDetailsUrl = ctwc.get(ctwc.URL_INSTANCE_SUMMARY, instanceUUID),
+                networkFQN = viewConfig['networkFQN'],
+                tabsToDisplay = viewConfig['tabsToDisplay'],
+                tabObjs = [];
+            var allTabs = [
+                        {
+                            elementId: ctwl.INSTANCE_DETAILS_ID,
+                            title: ctwl.TITLE_DETAILS,
+                            view: "DetailsView",
+                            viewConfig: {
+                                ajaxConfig: {
+                                    url: instanceDetailsUrl,
+                                    type: 'GET'
+                                },
+                                modelKey: ctwc.get(ctwc.UMID_INSTANCE_UVE, instanceUUID),
+                                templateConfig: ctwu.getInstanceDetailsTemplateConfig(),
+                                app: cowc.APP_CONTRAIL_CONTROLLER,
+                                dataParser: function (response) {
+                                    return {
+                                        name: instanceUUID,
+                                        value: response
+                                    };
+                                }
+                            }
+                        },
+                        {
+                            elementId: ctwl.INSTANCE_INTERFACE_ID,
+                            title: ctwl.TITLE_INTERFACES,
+                            view: "InterfaceGridView",
+                            app: cowc.APP_CONTRAIL_CONTROLLER,
+                            viewConfig: {
+                                modelKey: ctwc.get(ctwc.UMID_INSTANCE_UVE, instanceUUID),
+                                instanceUUID: instanceUUID,
+                                networkFQN: networkFQN
+                            }
+                        },
+                        {
+                            elementId: ctwl.INSTANCE_TRAFFIC_STATS_ID,
+                            title: ctwl.TITLE_TRAFFIC_STATISTICS,
+                            app: cowc.APP_CONTRAIL_CONTROLLER,
+                            view: "InstanceTrafficStatsView",
+                            viewConfig: {
+                                modelKey: ctwc.get(ctwc.UMID_INSTANCE_UVE, instanceUUID),
+                                instanceUUID: instanceUUID,
+                                parseFn: ctwp.parseLineChartData
+                            }
+                        },
+                        {
+                            elementId: ctwl.INSTANCE_PORT_HEAT_CHART_ID,
+                            title: ctwl.TITLE_PORT_MAP,
+                            view: "HeatChartView",
+                            viewConfig: {
+                                ajaxConfig: {
+                                    url: ctwc.get(ctwc.URL_INSTANCE_SUMMARY, instanceUUID),
+                                    type: 'GET'
+                                },
+                                chartOptions: {getClickFn: self.getHeatChartClickFn}
+                            }
+                        }
+                    ];
+            if (tabsToDisplay == null) {
+                tabObjs = allTabs;
+            } else if (typeof tabsToDisplay =='string' || $.isArray(tabsToDisplay)) {
+                if(typeof tabsToDisplay == 'string') {
+                    tabsToDisplay = [tabsToDisplay];
+                }
+                for(var i = 0; i < tabsToDisplay.length; i++ ) {
+                    $.each(allTabs,function(idx,obj) {
+                        if(obj['view'] == tabsToDisplay[i])
+                            tabObjs.push(obj);
+                    });
+                }
+            }
+            return {
+                elementId: cowu.formatElementId([ctwl.MONITOR_INSTANCE_VIEW_ID, '-section']),
+                view: "SectionView",
+                viewConfig: {
+                    rows: [
+                        {
+                            columns: [
+                                {
+                                    elementId: ctwl.INSTANCE_TABS_ID,
+                                    view: "TabsView",
+                                    viewConfig: {
+                                        theme: 'classic',
+                                        activate: function (e, ui) {
+                                            var selTab = $(ui.newTab.context).text();
+                                            if (selTab == ctwl.TITLE_TRAFFIC_STATISTICS) {
+                                                $('#' + ctwl.INSTANCE_TRAFFIC_STATS_ID).find('svg').trigger('refresh');
+                                            } else if (selTab == ctwl.TITLE_INTERFACES) {
+                                                $('#' + ctwl.INSTANCE_INTERFACE_GRID_ID).data('contrailGrid').refreshView();
+                                            }
+                                        },
+                                        tabs: tabObjs
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        
+        }
+        self.getInstanceDetailsTemplateConfig = function () {
+            return {
+
+                templateGenerator: 'RowSectionTemplateGenerator',
+                templateGeneratorConfig: {
+                    rows: [
+                        {
+                            templateGenerator: 'ColumnSectionTemplateGenerator',
+                            templateGeneratorConfig: {
+                                columns: [
+                                    {
+                                        class: 'span6',
+                                        rows: [
+                                            {
+                                                title: ctwl.TITLE_INSTANCE_DETAILS,
+                                                templateGenerator: 'BlockListTemplateGenerator',
+                                                templateGeneratorConfig: [
+                                                    {
+                                                        key: 'vm_name',
+                                                        templateGenerator: 'TextGenerator'
+                                                    },
+                                                    {
+                                                        key: 'value.UveVirtualMachineAgent.uuid',
+                                                        templateGenerator: 'TextGenerator'
+                                                    },
+                                                    {
+                                                        key: 'value.UveVirtualMachineAgent.vrouter',
+                                                        templateGenerator: 'TextGenerator'
+                                                    },
+                                                    {
+                                                        key: 'value.UveVirtualMachineAgent.interface_list',
+                                                        templateGenerator: 'TextGenerator',
+                                                        templateGeneratorConfig: {
+                                                            formatter: 'length'
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        class: 'span6',
+                                        rows: [
+                                            {
+                                                title: ctwl.TITLE_CPU_MEMORY_INFO,
+                                                templateGenerator: 'BlockListTemplateGenerator',
+                                                templateGeneratorConfig: [
+                                                    {
+                                                        key: 'value.UveVirtualMachineAgent.cpu_info.cpu_one_min_avg',
+                                                        templateGenerator: 'TextGenerator'
+                                                    },
+                                                    {
+                                                        key: 'value.UveVirtualMachineAgent.cpu_info.rss',
+                                                        templateGenerator: 'TextGenerator',
+                                                        templateGeneratorConfig: {
+                                                            formatter: 'kilo-byte'
+                                                        }
+                                                    },
+                                                    {
+                                                        key: 'value.UveVirtualMachineAgent.cpu_info.vm_memory_quota',
+                                                        templateGenerator: 'TextGenerator',
+                                                        templateGeneratorConfig: {
+                                                            formatter: 'kilo-byte'
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            
+            }
+        }
+        self.getInstanceTabViewModelConfig = function (instanceUUID) {
+            var modelKey = ctwc.get(ctwc.UMID_INSTANCE_UVE, instanceUUID);
+            var viewModelConfig = {
+                    modelKey: modelKey,
+                    remote: {
+                        ajaxConfig: {
+                            url: ctwc.get(ctwc.URL_INSTANCE_SUMMARY, instanceUUID),
+                            type: 'GET'
+                        },
+                        dataParser: function(response) {
+                            return {name: instanceUUID, value: response};
+                        }
+                    },
+                    vlRemoteConfig: {
+                        vlRemoteList: ctwgc.getVMInterfacesLazyRemoteConfig()
+                    }
+                };
+
+                return new ContrailViewModel(viewModelConfig);
+        }
+        
         self.getUUIDByName = function (fqName) {
             var fqArray = fqName.split(":"),
                 ucid, modeltems, cachedData;
