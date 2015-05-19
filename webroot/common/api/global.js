b79ee297ec1f2574ca9282754eccc282488b7e20
Manoj Naik <mnaik@juniper.net>
2015-05-19 19:08:51 +0530

    Related-Bug: #1429726 - Added db usage charts for db node details
    Change-Id: I7b66835e4cca7040bcf607e36881c0f972ca0c48

--- webroot/common/api/global.js
+++ webroot/common/api/global.js
@@ -26,12 +26,12 @@ global.QUERY_JSON = {
         "SUM(ifStats.ifOutPkts)"],
         "where":[[{"name":"name","value":"","op":1,"value2":null,
             "suffix":{"name":"ifStats.ifIndex","value":"","suffix":null,"op":1,"value2":null}}]]},
-    'StatTable.DatabaseUsageInfo.database_usage_stats': {
-        "table": "StatTable.DatabaseUsageInfo.database_usage_stats",
+    'StatTable.DatabaseUsageInfo.database_usage': {
+        "table": "StatTable.DatabaseUsageInfo.database_usage",
         "start_time": "", "end_time": "", "select_fields":
-            ["T","database_usage_stats.disk_space_used_1k",
-             "database_usage_stats.disk_space_available_1k",
-             "database_usage_stats.analytics_db_size_1k"],
+            ["T","database_usage.disk_space_used_1k",
+             "database_usage.disk_space_available_1k",
+             "database_usage.analytics_db_size_1k"],
          "where":[[{"name":"Source","op":1,"value":""}]]}
 }
 
