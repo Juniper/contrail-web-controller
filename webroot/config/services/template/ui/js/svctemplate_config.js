19123db6c9cdfab70f73f1341825aa26b10b70ef
balamurugang <gbala@juniper.net>
2015-05-27 16:34:07 +0530

    BugId 1459192 progress icon is shown until all data is loaded.
    Issue
    The icon was getting reset when the data is set in the grid because of the
    loading icon was not getting shown.
    
    Fixed:
    Loading the data to the grid first and then setting the loading status.
    So that the loading Icon is keep getting shown.
    Once all the data is loaded we are not setting the loading message.
    The Logic in Port and VN are different.
    VN : until the network is fetched showing the grid once the Shared network call
    is done Removing the message.
    Port : showing untill all the UUID call is getting called in the last call
    checking if UUIDArray length is less than or equal to 0 to remove the message.
    
    Impliemented pages
    Floating IP, IP Address Management, Logical Router, Policy, Port, Virtual
    Network, Service Template, Security Group,
    
    Change-Id: If72147b112f5e33f71ed0f87e2b96b62e8055803

--- webroot/config/services/template/ui/js/svctemplate_config.js
+++ webroot/config/services/template/ui/js/svctemplate_config.js
@@ -662,11 +662,12 @@ function successHandlerForGridsTempRow(result) {
         });
     }
     $("#gridsvcTemplate").data("contrailGrid")._dataView.setData(svcTemplateData);
-    if(!svcTemplateData || svcTemplateData.length<=0)
-        gridsvcTemplate.showGridMessage('empty');
     if(result.more == true || result.more == "true"){
         gridsvcTemplate = $("#gridsvcTemplate").data("contrailGrid");
-        gridsvcTemplate.showGridMessage('updating');
+        gridsvcTemplate.showGridMessage('loading');
+    } else {
+        if(!svcTemplateData || svcTemplateData.length<=0)
+            gridsvcTemplate.showGridMessage('empty');
     }
 }
 
