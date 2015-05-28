8d93c64befb02ac93ff6bbc9aa1bbd14340cdaf9
balamurugang <gbala@juniper.net>
2015-05-28 16:10:58 +0530

    Changed the count of fetching the data for the get for grid.
    In the grid data fetch get call the UUID count was 200 all time.
    Changed it to get 50 uuid record for the first call and then 200 for the next next calls.
    
    Affected pages:
    - Port
    - Virtual network
    
    Change-Id: Iad695fcbf4fcacff2c26f451f028b0e893a8fdcc
    Related-Bug: #1456972
    Related-Bug: #1456970

--- webroot/config/vn/ui/js/vn_config.js
+++ webroot/config/vn/ui/js/vn_config.js
@@ -48,7 +48,6 @@ function VirtualNetworkConfig() {
     var ajaxParam;
     var dynamicID;
     var selectedProuters;
-    var getUUIDCallCount;
     var vxlan_identifier_mode;
 
     //Method definitions
@@ -80,7 +79,6 @@ function VirtualNetworkConfig() {
     this.getDNSStatus = getDNSStatus;
     this.getAllDNSServer = getAllDNSServer;
     this.dynamicID = dynamicID;
-    this.getUUIDCallCount = getUUIDCallCount;
     this.vxlan_identifier_mode = vxlan_identifier_mode;
     this.destroy = destroy;
 }
@@ -107,7 +105,6 @@ function fetchData() {
 
 function initComponents() {
     dynamicID = 0;
-    getUUIDCallCount = 200;
     vxlan_identifier_mode = "automatic";
     $("#gridVN").contrailGrid({
         header : {
@@ -1900,10 +1897,10 @@ function successHandlerForAllUUIDGet(allUUID, cbparam)
         var vnUUIDObj = {};
         var sendUUIDArr = [];
         vnUUIDObj.type = "virtual-network";
-        sendUUIDArr = allUUID.slice(0, getUUIDCallCount);
+        sendUUIDArr = allUUID.slice(0, 50);
         vnUUIDObj.uuidList = sendUUIDArr;
         vnUUIDObj.fields = ["floating_ip_pools"];
-        allUUID = allUUID.slice(getUUIDCallCount, allUUID.length);
+        allUUID = allUUID.slice(50, allUUID.length);
         var param = {};
         param.cbparam = cbparam;
         param.allUUID = allUUID;
@@ -1930,10 +1927,10 @@ function successHandlerForGridVNLoop(result, param){
         var vnUUIDObj = {};
         var sendUUIDArr = [];
         vnUUIDObj.type = "virtual-network";
-        sendUUIDArr = allUUID.slice(0, getUUIDCallCount);
+        sendUUIDArr = allUUID.slice(0, 200);
         vnUUIDObj.uuidList = sendUUIDArr;
         vnUUIDObj.fields = ["floating_ip_pools"];
-        allUUID = allUUID.slice(getUUIDCallCount, allUUID.length);
+        allUUID = allUUID.slice(200, allUUID.length);
         var param = {};
         param.cbparam = cbparam;
         param.allUUID = allUUID;
