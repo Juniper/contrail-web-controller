fe141afcd6acabbd965e7e2d6f7ecdb87850b2c2
Naga Kiran <nagakiran@juniper.net>
2015-06-01 19:36:36 +0530

    Closes-Bug: #1460668 Issue is Infra dashboard loading is slow on setup with 4k+ networks. Found the issue is that vRouter UVE gives "connected_networks" key which is an array of all networks on that vRouter and with 4k+ networks, UVE size becomes huge and leads to lot of time to download response data from web server.
    Instead of  fetching "connected_networks" key, started fetching "vn_count",
    which gives network count on that vRouter.
    
    Also, getting the vn count on Infra dashboard from the following call
    instead of "connected_networks" key on vRouter:
    http://<ipAddress>:8081/analytics/virtual-networks?cfilt=UveVirtualNetworkConfig
    
    From this list, excluding the default-project networks and __UNKNOWN__.
    
    Change-Id: Ie5b5826d4df38ceb0db982a24fc6b427e568f0c7

--- webroot/monitor/tenant-network/api/network.mon.api.js
+++ webroot/monitor/tenant-network/api/network.mon.api.js
@@ -2806,6 +2806,21 @@ function getVirtualNetworksDetails (req, res, appData)
     });
 }
 
+//Returns the list of virtual networks for calculating the 
+//vn count in Infra Dashboard
+function getVirtualNetworksList (req, res, appData)
+{
+    var reqPostData = req.body;
+    var url = '/analytics/uves/virtual-networks?cfilt=UveVirtualNetworkAgent';
+    opApiServer.apiGet(url, appData, function(error, data) {
+        if (error) {
+            commonUtils.handleJSONResponse(error, res, null);
+            return;
+         }
+         commonUtils.handleJSONResponse(error, res, data);
+    });
+}
+
 function getInstanceDetails (req, res, appData)
 {
     var getVMCB = instanceDetailsMap[req.session.loggedInOrchestrationMode];
@@ -3108,3 +3123,4 @@ exports.getStats = getStats;
 exports.getvnStatsPerVrouter = getvnStatsPerVrouter;
 exports.getAllVirtualMachines = getAllVirtualMachines;
 exports.getVirtualMachinesFromConfig = getVirtualMachinesFromConfig;
+exports.getVirtualNetworksList = getVirtualNetworksList;
