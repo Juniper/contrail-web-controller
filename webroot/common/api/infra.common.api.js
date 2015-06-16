9ba30dacbf977be593aed40e8d75473ca7fe2acf
Biswajit Mandal <bmandal@juniper.net>
2015-06-16 09:08:02 +0530

    Closes-Bug: #1465477 Make consistenet error messages in vrouter different tabs. Removing some unwanted log messages.
    Change-Id: Icc5234feff4f02409cc4e014045e4e61ceb0ecba

--- webroot/common/api/infra.common.api.js
+++ webroot/common/api/infra.common.api.js
@@ -575,12 +575,12 @@ function sortUVEList (uveEntry1, uveEntry2)
 function sendSandeshRequest (req, res, dataObjArr, restAPI)
 {
     async.map(dataObjArr,
-              commonUtils.getServerRespByRestApi(restAPI, true),
+              commonUtils.getServerRespByRestApi(restAPI, false),
               function(err, data) {
-        if (data) {
+        if ((null == err) && (null != data)) {
             commonUtils.handleJSONResponse(null, res, data);
         } else {
-            commonUtils.handleJSONResponse(null, res, []);
+            sendServerRetrieveError(res);
         }
     });
 }
@@ -1004,6 +1004,12 @@ function getvRtrIntrospectPortByJobData (jobData)
     return global.SANDESH_COMPUTE_NODE_PORT;
 }
 
+function sendServerRetrieveError (res)
+{
+    var error = new appErrors.RESTServerError(global.STR_CACHE_RETRIEVE_ERROR);
+    commonUtils.handleJSONResponse(error, res, null);
+}
+
 exports.dovRouterListProcess = dovRouterListProcess;
 exports.checkAndGetSummaryJSON = checkAndGetSummaryJSON;
 exports.getvRouterList = getvRouterList;
@@ -1027,3 +1033,5 @@ exports.getvRouetrIntrospectPort = getvRouetrIntrospectPort;
 exports.fillIntrospectPortInJobData = fillIntrospectPortInJobData;
 exports.getvRouetrIntrospectPortByReq = getvRouetrIntrospectPortByReq;
 exports.getvRtrIntrospectPortByJobData = getvRtrIntrospectPortByJobData;
+exports.sendServerRetrieveError = sendServerRetrieveError;
+
