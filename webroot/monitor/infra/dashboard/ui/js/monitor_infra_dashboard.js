d08f7c9ab5b22d4ddad194e8d214e41013de61a3
Vishnu Vardhan V <vishnuvv@juniper.net>
2015-06-17 18:37:52 +0530

    Related-Bug:#1466079 Issue is the VRF dropdown in the infra details page routes tab is not listing all the VRF, Reason is VRF's from introspect coming in chunks,parsing logic considering the first chunk,now we changed the logic which will consider all the chunks.
    Change-Id: I9e1172debaf2f05a3ae5840c4856cf0fc3120ed3

--- webroot/monitor/infra/dashboard/ui/js/monitor_infra_dashboard.js
+++ webroot/monitor/infra/dashboard/ui/js/monitor_infra_dashboard.js
@@ -62,16 +62,18 @@ function addTabs() {
                     type:'POST',
                     data:{}
                 }).done(function(response) {
-                    $.each(response,function(idx,obj) {
-                        if((obj['name'].indexOf(':default-project:') == -1) && obj['name'] != '__UNKNOWN__') {
-                            if($.isNumeric(vnCnt)) {
-                                vnCnt++;
-                            } else {
-                                vnCnt = 1;
+                    if(response != null) {
+                        $.each(response,function(idx,obj) {
+                            if((obj['name'].indexOf(':default-project:') == -1) && obj['name'] != '__UNKNOWN__') {
+                                if($.isNumeric(vnCnt)) {
+                                    vnCnt++;
+                                } else {
+                                    vnCnt = 1;
+                                }
                             }
-                        }
-                    });
-                    $('#infobox-vns .infobox-data-number').text(vnCnt);
+                        });
+                        $('#infobox-vns .infobox-data-number').text(vnCnt);
+                    }
                 });
             }
 
