2055bd5f602028c51ed2965b1286eba58a21d185
Biswajit Mandal <bmandal@juniper.net>
2015-08-06 14:27:50 +0530

    Related-Bug: #1477699 Adding validation of dns/ip_fabric_service_ip based on the IP/DNS used. Removing validateAttr as it is not required, now checking the IP/DNS using required callback functionality.
    Change-Id: I3aeaf7f837621509bdf681b5676d99910ce8fd14

--- webroot/config/linklocalservices/ui/js/models/LinkLocalServicesModel.js
+++ webroot/config/linklocalservices/ui/js/models/LinkLocalServicesModel.js
@@ -17,38 +17,6 @@ define([
             'ip_fabric_service_ip': [],
             'def_visible': true
         },
-        validateAttr: function(attributePath, validation, data) {
-            var needValidate = true;
-            var model = this.model();
-            var isValid = true;
-            var attr = cowu.getAttributeFromPath(attributePath);
-            var attributes = model.attributes;
-            var errors = model.get(cowc.KEY_MODEL_ERRORS);
-            var attrErrorObj = {};
-
-            switch (attributePath) {
-            case 'ip_fabric_service_ip':
-                if ('DNS' == attributes['lls_fab_address_ip']) {
-                    needValidate = false;
-                }
-                break;
-            case 'ip_fabric_DNS_service_name':
-                if ('IP' == attributes['lls_fab_address_ip']) {
-                    needValidate = false;
-                }
-                break;
-            default:
-                break;
-            }
-            if (true == needValidate) {
-                isValid = model.isValid(attributePath, validation);
-            } else {
-                isValid = true;
-            }
-            attrErrorObj[attr + cowc.ERROR_SUFFIX_ID] =
-                (isValid == true) ? false : isValid;
-            errors.set(attrErrorObj);
-        },
         validations: {
             llsConfigValidations: {
                 'linklocal_service_name': {
@@ -66,12 +34,16 @@ define([
                     pattern: 'number',
                 },
                 'ip_fabric_service_ip': {
-                    required: false,
-                    pattern: cowc.PATTERN_IP_ADDRESS,
+                    required: function(val, attr, computed) {
+                        return ('IP' == computed['lls_fab_address_ip']);
+                    },
+                    pattern: cowc.PATTERN_IP_ADDRESS
                 },
                 'ip_fabric_DNS_service_name': {
-                    required: false,
-                    pattern: cowc.PATTERN_IP_ADDRESS,
+                    required: function(val, attr, computed) {
+                        return ('DNS' == computed['lls_fab_address_ip']);
+                    },
+                    pattern: cowc.PATTERN_IP_ADDRESS
                 },
                 'ip_fabric_service_port': {
                     required: true,
@@ -86,82 +58,82 @@ define([
             var ajaxConfig = {}, returnFlag = false;
 
             if (this.model().isValid(true, "llsConfigValidations")) {
-            var locks = this.model().attributes.locks.attributes;
-            var uuid = putData['global-vrouter-config']['uuid'];
-            var newLLSData = this.model().attributes;
+                var locks = this.model().attributes.locks.attributes;
+                var uuid = putData['global-vrouter-config']['uuid'];
+                var newLLSData = this.model().attributes;
 
-            if (!(newLLSData['ip_fabric_service_ip'] instanceof Array)) {
-                newLLSData['ip_fabric_service_ip'] =
-                    [newLLSData['ip_fabric_service_ip']];
-            }
-            newLLSData['ip_fabric_service_port'] =
-                parseInt(newLLSData['ip_fabric_service_port']);
-            newLLSData['linklocal_service_port'] =
-                parseInt(newLLSData['linklocal_service_port']);
-            if (newLLSData['lls_fab_address_ip'] == 'DNS') {
-                newLLSData['ip_fabric_service_ip'] = [];
-            } else {
-                newLLSData['ip_fabric_DNS_service_name'] = "";
-            }
-
-            ajaxConfig = {};
-            if (null == gridData) {
-                gridData = [];
-            }
-            if (-1 == rowIndex) {
-                /* Add */
-                gridData.push(newLLSData);
-                putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'] = gridData;
-            } else {
-                /* Edit */
-                putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'] = gridData;
-                putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'][rowIndex] = newLLSData;
-            }
-            var dataLen =
-                putData['global-vrouter-config']['linklocal_services']
-                ['linklocal_service_entry'].length;
-            for (var i = 0; i < dataLen; i++) {
-                delete
-                    putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'][i]['cgrid'];
-                delete
-                    putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'][i]['lls_fab_address_ip'];
-                delete
-                    putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'][i]['errors'];
-                delete
-                    putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'][i]['locks'];
-                delete
-                    putData['global-vrouter-config']['linklocal_services']
-                    ['linklocal_service_entry'][i]['def_visible'];
-            }
-            ajaxConfig.async = false;
-            ajaxConfig.type = "PUT";
-            ajaxConfig.data = JSON.stringify(putData);
-            ajaxConfig.url = '/api/tenants/config/global-vrouter-config/' +
-                uuid + '/link-local-services';
-            contrail.ajaxHandler(ajaxConfig, function () {
-                if (contrail.checkIfFunction(callbackObj.init)) {
-                    callbackObj.init();
+                if (!(newLLSData['ip_fabric_service_ip'] instanceof Array)) {
+                    newLLSData['ip_fabric_service_ip'] =
+                        [newLLSData['ip_fabric_service_ip']];
                 }
-            }, function (response) {
-                 console.log(response);
-                 if (contrail.checkIfFunction(callbackObj.success)) {
-                     callbackObj.success();
-                 }
-                 returnFlag = true;
-            }, function (error) {
-                 console.log(error);
-                 if (contrail.checkIfFunction(callbackObj.error)) {
-                     callbackObj.error(error);
-                 }
-                 returnFlag = false;
-            });
+                newLLSData['ip_fabric_service_port'] =
+                    parseInt(newLLSData['ip_fabric_service_port']);
+                newLLSData['linklocal_service_port'] =
+                    parseInt(newLLSData['linklocal_service_port']);
+                if (newLLSData['lls_fab_address_ip'] == 'DNS') {
+                    newLLSData['ip_fabric_service_ip'] = [];
+                } else {
+                    newLLSData['ip_fabric_DNS_service_name'] = "";
+                }
+
+                ajaxConfig = {};
+                if (null == gridData) {
+                    gridData = [];
+                }
+                if (-1 == rowIndex) {
+                    /* Add */
+                    gridData.push(newLLSData);
+                    putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'] = gridData;
+                } else {
+                    /* Edit */
+                    putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'] = gridData;
+                    putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'][rowIndex] = newLLSData;
+                }
+                var dataLen =
+                    putData['global-vrouter-config']['linklocal_services']
+                    ['linklocal_service_entry'].length;
+                for (var i = 0; i < dataLen; i++) {
+                    delete
+                        putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'][i]['cgrid'];
+                    delete
+                        putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'][i]['lls_fab_address_ip'];
+                    delete
+                        putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'][i]['errors'];
+                    delete
+                        putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'][i]['locks'];
+                    delete
+                        putData['global-vrouter-config']['linklocal_services']
+                        ['linklocal_service_entry'][i]['def_visible'];
+                }
+                ajaxConfig.async = false;
+                ajaxConfig.type = "PUT";
+                ajaxConfig.data = JSON.stringify(putData);
+                ajaxConfig.url = '/api/tenants/config/global-vrouter-config/' +
+                    uuid + '/link-local-services';
+                contrail.ajaxHandler(ajaxConfig, function () {
+                    if (contrail.checkIfFunction(callbackObj.init)) {
+                        callbackObj.init();
+                    }
+                }, function (response) {
+                    console.log(response);
+                    if (contrail.checkIfFunction(callbackObj.success)) {
+                        callbackObj.success();
+                    }
+                    returnFlag = true;
+                }, function (error) {
+                    console.log(error);
+                    if (contrail.checkIfFunction(callbackObj.error)) {
+                        callbackObj.error(error);
+                    }
+                    returnFlag = false;
+                });
             } else {
                 if (contrail.checkIfFunction(callbackObj.error)) {
                     callbackObj.error(this.getFormErrorText(ctwl.LINK_LOCAL_SERVICES_PREFIX_ID));
