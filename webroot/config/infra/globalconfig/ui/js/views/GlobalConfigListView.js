13dd2fd990cc24fd81b5aee94fa2099a34f0cc46
Biswajit Mandal <bmandal@juniper.net>
2015-09-09 07:26:02 +0530

    Related-Bug: #1487396 MVC code changes for Global Vrouter Config
    Change-Id: I3169d1c5e8f32d9a783068051322493c2d4f72fa

---
+++ webroot/config/infra/globalconfig/ui/js/views/GlobalConfigListView.js
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view',
+    'contrail-list-model',
+    'config/infra/globalconfig/ui/js/models/GlobalConfigModel'
+], function (_, ContrailView, ContrailListModel, GlobalConfigModel) {
+    var gridElId = "#" + ctwl.GLOBAL_CONFIG_GRID_ID;
+    var globalConfigObj = {};
+    var self = this;
+    var GlobalConfigListView = ContrailView.extend({
+        el: $(contentContainer),
+        render: function () {
+            var self = this, viewConfig = this.attributes.viewConfig;
+
+            var listModelConfig = {
+                remote: {
+                    ajaxConfig: {
+                        url: ctwc.get(ctwc.URL_GET_GLOBAL_VROUTER_CONFIG),
+                        type: "GET"
+                    },
+                    dataParser: globalConfigDataParser,
+                },
+                vlRemoteConfig :{
+                    vlRemoteList : vlRemoteGLConfig,
+                    completeCallback: function() {
+                        self.renderView4Config(self.$el, contrailListModel,
+                                   getGlobalConfigViewConfig(), null, null,
+                                   null, function() {
+                            $(gridElId).data('configObj', globalConfigObj);
+                        });
+                    }
+                },
+                cacheConfig: {
+                    ucid: 'global:config',
+                    cacheTimeout: 0
+                }
+            };
+
+            var contrailListModel = new ContrailListModel(listModelConfig);
+        }
+    });
+
+    var vlRemoteGLConfig = [
+        {
+            getAjaxConfig: function (responseJSON) {
+                var lazyAjaxConfig = {
+                    url: ctwc.get(ctwc.URL_GET_GLOBAL_ASN),
+                    type: "GET"
+                };
+                return lazyAjaxConfig;
+            },
+            successCallback: function(response, contrailListModel) {
+                var dataItems = [];
+                var globalASNData = response;
+                var dataItem = {};
+                var globalVRConfigMapLen = globalVRConfigMap.length;
+                for (var i = 2; i < globalVRConfigMapLen; i++) {
+                    var key = globalVRConfigMap[i]['key'];
+                    var value = response['global-system-config'][key];
+                    if (key == 'ip_fabric_subnets') {
+                        if (null == value) {
+                            dataItems.push({'name': globalVRConfigMap[i]['name'],
+                                           'value': {'subnet': []}, 'key': key});
+                            continue;
+                        }
+                        var subnet = value['subnet'];
+                        var subnetLen = subnet.length;
+                        var subnets = {'subnet': []};
+                        for (var j = 0; j < subnetLen; j++) {
+                            subnets['subnet'].push(subnet[j]['ip_prefix'] + "/" +
+                                         subnet[j]['ip_prefix_len']);
+                        }
+                        value = subnets;
+                    }
+                    dataItems.push({'name': globalVRConfigMap[i]['name'],
+                                    'value': value, 'key': key});
+                }
+                contrailListModel.addData(dataItems);
+                globalConfigObj['global-system-config'] =
+                    response['global-system-config'];
+            }
+        }
+    ];
+
+    var globalVRConfigMap = [
+        {'key': 'vxlan_network_identifier_mode',
+         'name': 'VxLAN Identifier Mode'},
+        {'key': 'encapsulation_priorities',
+         'name': 'Encapsulation Priority Order',},
+        {'key': 'autonomous_system', 'name': 'Global ASN'},
+        {'key': 'ibgp_auto_mesh', 'name': 'iBGP Auto Mesh'},
+        {'key': 'ip_fabric_subnets', 'name': 'IP Fabric Subnets'}
+    ];
+
+    var globalConfigDataParser = function (response) {
+        console.log("Inside :", response);
+        var dataItem = {};
+        var dataItems = [];
+        var globalVRConfigMapLen = globalVRConfigMap.length;
+        for (var i = 0; i < 2; i++) {
+            dataItems[i] = {};
+            var key = globalVRConfigMap[i]['key'];
+            dataItems[i]['name'] = globalVRConfigMap[i]['name'];
+            dataItems[i]['value'] = response['global-vrouter-config'][key];
+            dataItems[i]['key'] = key;
+            /*
+            if (key == 'encapsulation_priorities') {
+                dataItems[i]['value'] = dataItems[i]['value']['encapsulation'];
+            }
+            */
+        }
+        globalConfigObj['global-vrouter-config'] = response['global-vrouter-config'];
+        return dataItems;
+    }
+
+    var getGlobalConfigViewConfig = function () {
+        return {
+            elementId: cowu.formatElementId([ctwl.CONFIG_GLOBAL_CONFIG_SECTION_ID]),
+            view: "SectionView",
+            viewConfig: {
+                rows: [
+                    {
+                        columns: [
+                            {
+                                elementId: ctwl.CONFIG_GLOBAL_CONFIG_ID,
+                                title: ctwl.TITLE_GLOBAL_CONFIG,
+                                view: "GlobalConfigGridView",
+                                viewPathPrefix:
+                                    "config/infra/globalconfig/ui/js/views/",
+                                app: cowc.APP_CONTRAIL_CONTROLLER,
+                                viewConfig: {
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    };
+
+    return GlobalConfigListView;
+});
+
--- webroot/config/linklocalservices/api/globalvrouterconfig.api.js
+++ webroot/config/linklocalservices/api/globalvrouterconfig.api.js
@@ -127,6 +127,94 @@ function createGlobalvRouterConfig (request, response, appData) {
 }
 
 /**
+ * @updateGlobalConfig
+ * 1. URL /api/tenants/config/update-global-config
+ * 2. Updates global-vrouter-config and global-system-config and process
+ * data from config api server and sends back the http response.
+ */
+function updateGlobalConfig (req, res, appData)
+{
+    var error = null;
+    var dataObjArr = [];
+    var uiGlobalConfig = req.body;
+    if ((null == uiGlobalConfig) ||
+        (null == uiGlobalConfig['global-vrouter-config']) ||
+        (null == uiGlobalConfig['global-vrouter-config']['uuid']) ||
+        (null == uiGlobalConfig['global-system-config']) ||
+        (null == uiGlobalConfig['global-system-config']['uuid'])) {
+        error = new appErrors.RESTServerError('Invalid Request ');
+        commonUtils.handleJSONResponse(error, res, null);
+        return;
+    }
+    var globalVRId = uiGlobalConfig['global-vrouter-config']['uuid'];
+    var globalSCId = uiGlobalConfig['global-system-config']['uuid'];
+
+    var putGVCData = {'global-vrouter-config':
+        uiGlobalConfig['global-vrouter-config']};
+    var putGSCData = {'global-system-config':
+        uiGlobalConfig['global-system-config']};
+
+    dataObjArr.push({type: 'global-system-config', uuid: globalSCId,
+                    putData: putGSCData, appData: appData});
+    dataObjArr.push({type: 'global-vrouter-config', uuid: globalVRId,
+                    putData: putGVCData, appData: appData});
+    async.map(dataObjArr, updateGlobalConfigCB, function(err, data) {
+        commonUtils.handleJSONResponse(err, res, null);
+    });
+}
+
+function updateGlobalSystemConfigCB (dataObj, callback)
+{
+    var appData       = dataObj['appData'];
+    var config        = dataObj['config'];
+    var fqnameURL     = '/fqname-to-id';
+    var gscReqBody    = {
+        'fq_name': ['default-global-system-config'],
+        'type': 'global-system-config'
+    };
+    configApiServer.apiPost(fqnameURL, gscReqBody, appData,
+                            function(error, data) {
+        var gscURL = '/global-system-config/' + dataObj['uuid'];
+        configApiServer.apiGet(gscURL, appData, function(err, gscData) {
+            if ((null != err) || (null == gscData)) {
+                callback(err, gscData);
+                return;
+            }
+            var uiASN =
+                dataObj['putData']['global-system-config']
+                       ['autonomous_system'];
+            var diffObj =
+                jsonDiff.getConfigJSONDiff('global-system-config',
+                                           gscData, dataObj['putData']);
+            if (null == diffObj) {
+                return;
+            }
+            configApiServer.apiPut(gscURL, diffObj, appData,
+                                   function(error, data) {
+                if (null == uiASN) {
+                    callback(error, data);
+                    return;
+                }
+                setTimeout(function() {
+                    callback(error, data);
+                    return;
+                }, 3000);
+            });
+        });
+    });
+}
+
+function updateGlobalConfigCB (dataObj, callback)
+{
+    var type = dataObj['type'];
+    if (type == 'global-vrouter-config') {
+        updateForwardingOptionsCB(dataObj, callback);
+    } else {
+       updateGlobalSystemConfigCB(dataObj, callback);
+    }
+}
+
+/**
  * @updateForwardingOptions
  * 1. URL /api/tenants/config/global-vrouter-config/:id
  * 2. Updates 'vxlan_network_identifier_mode' and 'encapsulation_priorities' 
@@ -135,16 +223,29 @@ function createGlobalvRouterConfig (request, response, appData) {
  */
 function updateForwardingOptions (request, response, appData) {
     var gvrPutData = request.body;
+    var gvrId = request.param('id');
+    updateForwardingOptionsCB({uuid: gvrId, putData: gvrPutData,
+                              appData: appData},
+                              function(error, data) {
+        commonUtils.handleJSONResponse(error, response, data);
+    });
+}
+
+function updateForwardingOptionsCB (gvrObj, callback)
+{
+    var gvrPutData = gvrObj['putData'];
+    var appData = gvrObj['appData'];
+    var gvrId = gvrPutData['global-vrouter-config']['uuid'];
+
     if (!('global-vrouter-config' in gvrPutData)) {
         error = new appErrors.RESTServerError('Invalid Request ');
-        commonUtils.handleJSONResponse(error, response, null);
+        callback(error, null);
         return;
     }
 
-    var gvrId = request.param('id');
     if(null === gvrId || typeof gvrId === "undefined") {
         error = new appErrors.RESTServerError('Invalid ID');
-        commonUtils.handleJSONResponse(error, response, null);
+        callback(error, null);
         return;
     }
     
@@ -152,7 +253,7 @@ function updateForwardingOptions (request, response, appData) {
     configApiServer.apiGet(gvrGetURL, appData,
         function(error, data) {
            	if (error) {
-           		commonUtils.handleJSONResponse(error, response, null);
+                callback(error, null);
            		return;
            	} else {
         	    var gvrPutURL = '/global-vrouter-config/' + gvrId;
@@ -168,15 +269,15 @@ function updateForwardingOptions (request, response, appData) {
         	        configApiServer.apiPut(gvrPutURL, diffObj, appData,
         	            function (error, data) {
 		                	if (error) {
-		                		commonUtils.handleJSONResponse(error, response, null);
+                                callback(error, null);
 		                		return;
 		                	} else {
-		                		commonUtils.handleJSONResponse(error, response, data);
+                                callback(error, data);
 		                	}
 		                	return;
         	            });
            		} else {
-                    commonUtils.handleJSONResponse(error, response, null);
+                    callback(error, null);
                 }
             }
         });
@@ -247,3 +348,4 @@ exports.getGlobalvRouterConfig = getGlobalvRouterConfig;
 exports.createGlobalvRouterConfig = createGlobalvRouterConfig;
 exports.updateForwardingOptions = updateForwardingOptions;
 exports.updateLinkLocalService = updateLinkLocalService;
+exports.updateGlobalConfig = updateGlobalConfig;
