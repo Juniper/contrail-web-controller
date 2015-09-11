6dca31f85938a40b681c30a57b9977e4069b6ecc
Biswajit Mandal <bmandal@juniper.net>
2015-09-11 10:54:11 +0530

    Related-Bug: #1494586 MVC Code changes for Security Group Page Pending: 1. In edit pop-up, the selection of dropDownView does not work if the data is CIDR, because we get the same id for the contrail drop down, so we can not set the data accordingly.
    Change-Id: Ie8f985571f7638cfde7814eba0f07cc0f3697f68

---
+++ webroot/config/networking/securitygroup/ui/js/views/SecGrpView.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view'
+], function (_, ContrailView) {
+    var SecGrpView = ContrailView.extend({
+        el: $(contentContainer),
+        renderSecGrp: function (viewConfig) {
+            var self = this;
+            self.renderView4Config(self.$el, null, getSecGrpConfig(viewConfig));
+        }
+    });
+
+    function getSecGrpConfig (viewConfig) {
+        var hashParams = viewConfig.hashParams,
+            customProjectDropdownOptions = {
+                childView: {
+                    init: getSecGrp(viewConfig),
+                },
+                allDropdownOption: ctwc.ALL_PROJECT_DROPDOWN_OPTION
+            },
+            customDomainDropdownOptions = {
+                childView: {
+                    init: getProjectBreadcrumbDropdownViewConfig(hashParams,
+                                                                 customProjectDropdownOptions)
+                }
+            };
+        return getDomainBreadcrumbDropdownViewConfig(hashParams,
+                                                     customDomainDropdownOptions)
+    };
+
+    function getDomainBreadcrumbDropdownViewConfig(hashParams,
+                                                   customDomainDropdownOptions) {
+        var urlValue =
+            (contrail.checkIfKeyExistInObject(true, hashParams,
+                                              'focusedElement.fqName') ?
+             hashParams.focusedElement.fqName : null),
+            defaultDropdownoptions = {
+                urlValue: (urlValue !== null) ?
+                              urlValue.split(':').splice(0,1).join(':') : null,
+                cookieKey: cowc.COOKIE_DOMAIN
+            },
+            dropdownOptions = $.extend(true, {},
+                                       defaultDropdownoptions,
+                                       customDomainDropdownOptions);
+
+        return {
+            elementId: ctwl.DOMAINS_BREADCRUMB_DROPDOWN,
+            view: "BreadcrumbDropdownView",
+            viewConfig: {
+                modelConfig: ctwu.getDomainListModelConfig(),
+                dropdownOptions: dropdownOptions
+            }
+        }
+    }
+
+    function getProjectBreadcrumbDropdownViewConfig(hashParams,
+                                                    customProjectDropdownOptions) {
+        var urlValue =
+            (contrail.checkIfKeyExistInObject(true, hashParams,
+                                              'focusedElement.fqName') ?
+             hashParams.focusedElement.fqName : null);
+
+        return function(domainSelectedValueData) {
+            var domain = domainSelectedValueData.name,
+                defaultDropdownOptions = {
+                    urlValue: (urlValue !== null) ?
+                                  urlValue.split(':').splice(1, 1).join(':') : null,
+                    cookieKey: cowc.COOKIE_PROJECT
+                },
+                dropdownOptions = $.extend(true, {},
+                                           defaultDropdownOptions,
+                                           customProjectDropdownOptions);
+
+            return {
+                elementId: ctwl.PROJECTS_BREADCRUMB_DROPDOWN,
+                view: "BreadcrumbDropdownView",
+                viewConfig: {
+                    modelConfig: ctwu.getProjectListModelConfig(domain),
+                    dropdownOptions: dropdownOptions
+                }
+            }
+        };
+    }
+
+    function getSecGrp(viewConfig) {
+        return function (projectSelectedValueData) {
+            return {
+                elementId: cowu.formatElementId([ctwl.CONFIG_QUOTAS_PAGE_ID]),
+                view: "SecGrpListView",
+                app: cowc.APP_CONTRAIL_CONTROLLER,
+                viewPathPrefix: "config/networking/securitygroup/ui/js/views/",
+                viewConfig: $.extend(true, {}, viewConfig,
+                                 {projectSelectedValueData: projectSelectedValueData})
+            }
+        }
+    };
+    return SecGrpView;
+});
+
