122fb496f6a2df8582072e0e22dec5349cf3f5cd
Sarin Kizhakkepurayil <sarink@juniper.net>
2015-10-06 11:49:47 -0700

    Related-Bug: #1470231 UT enhancements 1. fixed issue loading graph and nvd3 chart on test pages 2. new instanceView test page and mock data. Tests detailsView, LineBarWithFocusChart, tabsView and GridView common tests 3. updated Grunt task to run the new test. added as seperate tasks since appending to existing task runs into error state [needs to debug]. 4. new common data generator function for details view. 5. details view test suite config should pass model config for mock data creation
    Change-Id: I34f060393b622c0f4c9ff4f94b7f40ea3ff75a99

--- webroot/test/ui/ct.test.utils.js
+++ webroot/test/ui/ct.test.utils.js
@@ -3,8 +3,9 @@
  */
 define([
     'co-test-utils',
-    'contrail-list-model'
-], function (cotu, ContrailListModel) {
+    'contrail-list-model',
+    'contrail-view-model'
+], function (cotu, ContrailListModel, ContrailViewModel) {
 
     this.getRegExForUrl = function (url) {
         var regexUrlMap = {
@@ -12,7 +13,8 @@ define([
             '/api/tenants/config/projects': /\/api\/tenants\/config\/projects.*$/,
             '/api/tenant/networking/virtual-networks/details': /\/api\/tenant\/networking\/virtual-networks\/details\?.*$/,
             '/api/tenant/networking/stats': /\/api\/tenant\/networking\/stats.*$/,
-            '/api/tenant/networking/virtual-machine-interfaces/summary' : /\/api\/tenant\/networking\/virtual-machine-interfaces\/summary.*$/
+            '/api/tenant/networking/virtual-machine-interfaces/summary': /\/api\/tenant\/networking\/virtual-machine-interfaces\/summary.*$/,
+            '/api/admin/reports/query' : /\/api\/admin\/reports\/query.*$/,
         };
 
         return regexUrlMap [url];
@@ -34,11 +36,34 @@ define([
         return dataArr;
     };
 
+    this.commonDetailsDataGenerator = function (viewObj, defObj) {
+        var viewConfig = cotu.getViewConfigObj(viewObj),
+            modelMap = viewObj.modelMap,
+            modelData = viewConfig.data,
+            ajaxConfig = viewConfig.ajaxConfig,
+            dataParser = viewConfig.dataParser,
+            contrailViewModel;
+
+        if (modelMap != null && modelMap[viewConfig.modelKey] != null) {
+            contrailViewModel = modelMap[viewConfig.modelKey];
+            defObj.resolve();
+        } else {
+            var modelRemoteDataConfig = {
+                remote: {
+                    ajaxConfig: ajaxConfig,
+                    dataParser: dataParser
+                }
+            };
+            contrailViewModel = new ContrailViewModel($.extend(true, {data: modelData}, modelRemoteDataConfig));
+        }
+        return contrailViewModel;
+    }
 
     return {
         self: self,
         getRegExForUrl: getRegExForUrl,
         commonGridDataGenerator: commonGridDataGenerator,
+        commonDetailsDataGenerator: commonDetailsDataGenerator,
         deleteSizeField: deleteSizeField
     };
 
