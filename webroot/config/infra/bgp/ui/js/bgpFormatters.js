5fc1cdc541c44196b6f87928406b8d279b973350
Siva Bavanasi <kbsiva@juniper.net>
2015-12-28 15:04:55 +0530

    Added new Attributes to the UI as for schema
    and also fixed below issues
    1)Collection validations for peers
    2)Able to click on Delete button even though it is disabled
    3)No Peer selected Message popup should be shown if no peer is selected
    
    Closes-bug: #1529582
    Closes-bug: #1498770
    Closes-bug: #1513341
    Closes-bug: #1475370
    
    Change-Id: I4cefe150bf246b3fa809b076b5659850e9d9eb1b

--- webroot/config/infra/bgp/ui/js/bgpFormatters.js
+++ webroot/config/infra/bgp/ui/js/bgpFormatters.js
@@ -7,19 +7,43 @@ define(['underscore'], function(_){
         var self = this;
 
         /*
-         * @roleFormatter
+         * @displayNameFormatter
          */
-        self.roleFormatter = function(r, c, v, cd, dc) {
-            var role;
-            var vendor = getValueByJsonPath(dc,
-                'bgp_router_parameters;vendor', '');
-            if(vendor.trim().toLowerCase() === '' ||
-                vendor.trim().toLowerCase() === 'contrail') {
-                    role = 'Control Node';
-            } else {
-                role = 'BGP Router';
+        self.displayNameFormatter = function(r, c, v, cd, dc) {
+            var displayName = getValueByJsonPath(dc, "display_name", "");
+            if(!displayName){
+                displayName = getValueByJsonPath(dc, "name", "-");
             }
-            return role;
+            return displayName;
+        };
+
+        /*
+         * @routerTypeFormatter
+         */
+        self.routerTypeFormatter = function(r, c, v, cd, dc) {
+            var routerTypeDisplayName;
+            var routerType = getValueByJsonPath(dc, "bgp_router_parameters;router_type", "");
+            switch(routerType) {
+                case "control-node" :
+                    routerTypeDisplayName = "Control Node";
+                    break;
+                case "external-control-node" :
+                    routerTypeDisplayName = "External Control Node";
+                    break;
+                case "router" :
+                    routerTypeDisplayName = "BGP Router";
+                    break;
+                case "bgpaas-server" :
+                    routerTypeDisplayName = "BGP as a service Server";
+                    break;
+                case "bgpaas-client" :
+                    routerTypeDisplayName = "BGP as a service Client";
+                    break;
+                default :
+                    routerTypeDisplayName = "-";
+                    break;
+            }
+            return routerTypeDisplayName;
         };
 
         /*
@@ -48,14 +72,43 @@ define(['underscore'], function(_){
             var bgpRefs = getValueByJsonPath(dc,"bgp_router_refs", []);
             if(bgpRefs.length > 0){
                 peerString =
-                    "<table><tr><td style='width:200px;'>Name</td><td style='width:200px;'>Authentication Type</td></tr>";
+                    "<table style='width:500px !important;'><tr><th style='width:360px;'>Peer</th>\
+                    <th style='width:250px;'>State</th>\
+                    <th style='width:300px;'>Passive</th>\
+                    <th style='width:500px;'>Hold Time</th>\
+                    <th style='width:500px;'>Loop Count</th>\
+                    <th style='width:420px;'>Auth Mode</th></tr>";
                 bgpRefs.forEach(function(bgp){
-                    peerString += "<tr><td style='width:200px;'>";
-                    peerString += bgp.to[4]
-                    peerString += "</td><td style='width:200px;'>";
-                    peerString += cowu.getJSONValueByPath(
-                        'attr.session[0].attributes[0].auth_data.key_type', bgp)
+                    var attr = getValueByJsonPath(bgp,
+                        "attr;session;0;attributes;0", {});
+                    var state;
+                    var endTagStr = "</td><td style='width:300px;'>";
+                    peerString += "<tr><td>";
+                    peerString += bgp.to[4] + "</td><td>";
+                    state = getValueByJsonPath(attr, "admin_down", "-");
+                    if(state != "-") {
+                        state = state.toString() === "true" ? "Down" : "Up";
+                    }
+                    peerString += state + "</td><td>";
+                    peerString += getValueByJsonPath(attr, "passive", "-") + "</td><td>";
+                    var holdTime = getValueByJsonPath(attr, "hold_time", "-");
+                    if(holdTime != "-") {
+                        holdTime = holdTime + " (seconds)";
+                    }
+                    peerString += holdTime + "</td><td>";
+                    peerString += getValueByJsonPath(attr, "loop_count", "-") + "</td><td>";
+                    peerString += getValueByJsonPath(attr, 'auth_data;key_type', '-')
                     peerString += "</td></tr>";
+                    var familyAttrs = getValueByJsonPath(attr, "family_attributes", []);
+                    if(familyAttrs.length > 0) {
+                        peerString +="<tr><td class='addressFamily' colspan='6'><table width='100%'><thead><tr><th>Address Family</th><th>Loop Count</th><th>Prefix Limit</th></tr></thead><tbody>"
+                        for(var i =0 ; i < familyAttrs.length; i++) {
+                            var familyAttr = familyAttrs[i];
+                                peerString += "<tr><td>" + familyAttr.address_family + "</td><td>" +
+                                    familyAttr.loop_count + "</td><td>" + familyAttr.prefix_limit.maximum + "</td></tr>"
+                        }
+                        peerString += "</tbody></table></td></tr>";
+                    }
                 });
                 peerString += "</table>";
             } else {
@@ -120,6 +173,18 @@ define(['underscore'], function(_){
             }
             return holdTime;
         };
+
+        /*
+         * @stateFormatter
+         */
+         self.stateFormatter = function(r, c, v, cd, dc) {
+             var state = getValueByJsonPath(dc,
+                 "bgp_router_parameters;admin_down", "-");
+             if(state != "-") {
+                 state = state.toString() === "true" ? "Down" : "Up";
+             }
+             return state;
+         }
     };
     return bgpFormatters;
 });
