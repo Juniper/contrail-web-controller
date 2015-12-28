8d74510eee48c91b166c175508c8466e6ce6432e
Siva Bavanasi <kbsiva@juniper.net>
2015-12-28 15:04:55 +0530

    Added new Attributes to the UI as for schema
    and also fixed below issues
    1)Collection validations for peers
    2)Able to click on Delete button even though it is disabled
    3)No Peer selected Message popup should be shown if no peer is selected
    
    Closes-bug: #1529582
    Closes-bug: #1498770
    Closes-bug: #1513341
    Closes-bug: #1475370
    
    Change-Id: I4cefe150bf246b3fa809b076b5659850e9d9eb1b

--- webroot/config/infra/bgp/ui/js/bgpFormatters.js
+++ webroot/config/infra/bgp/ui/js/bgpFormatters.js
@@ -7,19 +7,43 @@ define(['underscore'], function(_){
         var self = this;
 
         /*
-         * @roleFormatter
+         * @displayNameFormatter
          */
-        self.roleFormatter = function(r, c, v, cd, dc) {
-            var role;
-            var vendor = getValueByJsonPath(dc,
-                'bgp_router_parameters;vendor', '');
-            if(vendor.trim().toLowerCase() === '' ||
-                vendor.trim().toLowerCase() === 'contrail') {
-                    role = 'Control Node';
-            } else {
-                role = 'BGP Router';
+        self.displayNameFormatter = function(r, c, v, cd, dc) {
+            var displayName = getValueByJsonPath(dc, "display_name", "");
+            if(!displayName){
+                displayName = getValueByJsonPath(dc, "name", "-");
             }
-            return role;
+            return displayName;
+        };
+
+        /*
+         * @routerTypeFormatter
+         */
+        self.routerTypeFormatter = function(r, c, v, cd, dc) {
+            var routerTypeDisplayName;
+            var routerType = getValueByJsonPath(dc, "bgp_router_parameters;router_type", "");
+            switch(routerType) {
+                case "control-node" :
+                    routerTypeDisplayName = "Control Node";
+                    break;
+                case "external-control-node" :
+                    routerTypeDisplayName = "External Control Node";
+                    break;
+                case "router" :
+                    routerTypeDisplayName = "BGP Router";
+                    break;
+                case "bgpaas-server" :
+                    routerTypeDisplayName = "BGP as a service Server";
+                    break;
+                case "bgpaas-client" :
+                    routerTypeDisplayName = "BGP as a service Client";
+                    break;
+                default :
+                    routerTypeDisplayName = "-";
+                    break;
+            }
+            return routerTypeDisplayName;
         };
 
         /*
@@ -48,15 +72,51 @@ define(['underscore'], function(_){
             var bgpRefs = getValueByJsonPath(dc,"bgp_router_refs", []);
             if(bgpRefs.length > 0){
                 peerString =
-                    "<table><tr><td style='width:200px;'>Name</td><td style='width:200px;'>Authentication Type</td></tr>";
+                    "<table style='width:1000px !important;'><tr><th style='width:200px;'>Peer</th>\
+                    <th style='width:200px;'>State</th>\
+                    <th style='width:200px;'>Passive</th>\
+                    <th style='width:250px;'>Hold Time (seconds)</th>\
+                    <th style='width:200px;'>Loop Count</th>\
+                    <th style='width:200px;'>Auth Mode</th>\
+                    <th style='width:800px;'>Family Attributes</th></tr>";
                 bgpRefs.forEach(function(bgp){
-                    peerString += "<tr><td style='width:200px;'>";
-                    peerString += bgp.to[4]
-                    peerString += "</td><td style='width:200px;'>";
-                    peerString += cowu.getJSONValueByPath(
-                        'attr.session[0].attributes[0].auth_data.key_type', bgp)
-                    peerString += "</td></tr>";
+                    var attr = getValueByJsonPath(bgp,
+                        "attr;session;0;attributes;0", {});
+                    var state;
+                    peerString += "<tr style='vertical-align:top'><td>";
+                    peerString += bgp.to[4] + "</td><td>";
+                    state = getValueByJsonPath(attr, "admin_down", "-");
+                    if(state != "-") {
+                        state = state.toString() === "true" ? "Down" : "Up";
+                    }
+                    peerString += state + "</td><td>";
+                    var passive = getValueByJsonPath(attr, "passive", "-");
+                    if(passive != "-") {
+                        passive = passive ? "True" : "False";
+                    }
+                    peerString += passive + "</td><td>";
+                    peerString += getValueByJsonPath(attr, "hold_time", "-") + "</td><td>";
+                    peerString += getValueByJsonPath(attr, "loop_count", "-") + "</td><td>";
+                    peerString += getValueByJsonPath(attr, 'auth_data;key_type', '-')
+                    peerString += "</td><td>";
+                    var familyAttrs = getValueByJsonPath(attr, "family_attributes", []);
+                    if(familyAttrs.length > 0) {
+                        for(var i = 0 ; i < familyAttrs.length; i++) {
+                            var familyAttr = familyAttrs[i];
+                            peerString += "Address Family : " + getValueByJsonPath(familyAttr, "address_family", "-");
+                            peerString += ", Loop Count : " + getValueByJsonPath(familyAttr, "loop_count", "-");
+                            peerString += ", Prefix Limit : " + getValueByJsonPath(familyAttr, "prefix_limit;maximum", "-");
+                            if(i === familyAttrs.length - 1) {
+                                peerString +="</br></br>";
+                            } else {
+                                peerString +="</br>";
+                            }
+                        }
+                    } else {
+                        peerString += "-";
+                    }
                 });
+                peerString += "</td></tr>";
                 peerString += "</table>";
             } else {
                 peerString = "-";
@@ -120,6 +180,18 @@ define(['underscore'], function(_){
             }
             return holdTime;
         };
+
+        /*
+         * @stateFormatter
+         */
+         self.stateFormatter = function(r, c, v, cd, dc) {
+             var state = getValueByJsonPath(dc,
+                 "bgp_router_parameters;admin_down", "-");
+             if(state != "-") {
+                 state = state.toString() === "true" ? "Down" : "Up";
+             }
+             return state;
+         }
     };
     return bgpFormatters;
 });
---
+++ webroot/config/infra/bgp/ui/js/models/bgpFamilyAttrsModel.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+ define(["underscore", "contrail-model"], function(_, ContrailModel){
+     var bgpFamilyAttrsModel = ContrailModel.extend({
+         defaultConfig: {
+             "address_family": null,
+             "loop_count": 0,
+             "prefix_limit": null
+         },
+         formatModelConfig: function(modelConfig){
+             return modelConfig
+         },
+         validateAttr: function (attributePath, validation, data) {
+             var model = data.model().attributes.model(),
+                 attr = cowu.getAttributeFromPath(attributePath),
+                 errors = model.get(cowc.KEY_MODEL_ERRORS),
+                 attrErrorObj = {}, isValid;
+
+             isValid = model.isValid(attributePath, validation);
+
+             attrErrorObj[attr + cowc.ERROR_SUFFIX_ID] = (isValid == true) ?
+                 false : isValid;
+             errors.set(attrErrorObj);
+         },
+         validations: {
+             familyAttrValidation : {
+                 "address_family" : {
+                     required: true,
+                     msg: "Adress family is required"
+                 },
+                 "loop_count" : function(value, attr, finalObj) {
+                     if(value) {
+                         if(isNaN(value) || Number(value) < 0 || Number(value) > 16) {
+                             return "Loop count should be in 0-16 range"
+                         }
+                     }
+                 },
+                 "prefix_limit" : function(value, attr, finalObj) {
+                     if(value && isNaN(value)) {
+                         return "Prefix limit should be a number";
+                     }
+                 }
+             }
+         }
+     });
+     return bgpFamilyAttrsModel;
+ });
