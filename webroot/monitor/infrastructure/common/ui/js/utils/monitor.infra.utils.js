1c76165f9faa89674324ca6c1a6c685ab1cace44
Vishnu Vardhan V <vishnuvv@juniper.net>
2016-01-11 19:47:27 +0530

    Related-Bug:#1532814 Did the following changes in underlay page, 1)Added the vRouter IP in the tooltip 2)Changed the UUID to name in VM tooltip title 3)When the map/trace flow response is empty nodes and links, model change event is not triggered because the default values are empty arrays hence we are triggering manually when the response is empty array of nodes and links. 4)Showing the appropriate message and resetting the earlier path when the trace/map flow response is empty. 5)Changes the Tab title from search flow to Map flow. 6)Removing the tabs related to node while showing the trace flow/map flow response. 7)Changed the grid title from Trace Flows to Flows in trace flow results. 8)Fixed the issue, tough the query returns the response we are not showing in the grid in trace flow VM case this is happening since the last commit. 9)Fixed the issue, loading icon is not hiding in case when we are not issuing the ajax call for the trace/map flow. Change-Id: Icbcb8904b60e850909d7376eed0f80ef5b095879

--- webroot/monitor/infrastructure/common/ui/js/utils/monitor.infra.utils.js
+++ webroot/monitor/infrastructure/common/ui/js/utils/monitor.infra.utils.js
@@ -2705,6 +2705,9 @@ define([
                 }
                 if(currentPage == 'mon_infra_underlay' &&
                     !graphModel.checkIPInVrouterList(params)) {
+                    if(deferredObj != null) {
+                        deferredObj.resolve(true);
+                    }
                     showInfoWindow(
                         "Cannot Map the path for the selected flow", "Info");
                     return;
@@ -2731,16 +2734,22 @@ define([
                                 return;
                             }
                             graphModel.underlayPathReqObj = params;
-                            graphModel.flowPath.set('links', response['links']);
-                            graphModel.flowPath.set('nodes', response['nodes']);
+                            graphModel.flowPath.set({
+                                'nodes': ifNull(response['nodes'], []),
+                                'links': ifNull(response['links'], [])
+                            });
+                            if (ifNull(response['nodes'], []).length == 0 ||
+                                ifNull(response['links'], []).length == 0) {
+                                graphModel.flowPath.trigger('change:nodes');
+                            }
                             $('html,body').animate({scrollTop:0}, 500);
                         }).fail (function () {
                             if(params['startAt'] != null &&
-                                    graphModel.lastInteracted > params['startAt']) {
-                                    if (deferredObj != null) {
-                                        deferredObj.resolve(false);
-                                    }
-                                    return;
+                                graphModel.lastInteracted > params['startAt']) {
+                                if (deferredObj != null) {
+                                    deferredObj.resolve(false);
+                                }
+                                return;
                             }
                             showInfoWindow('Error in fetching details','Error');
                         }).always (function (ajaxObj, state, error) {
