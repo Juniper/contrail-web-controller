20bec58ee9f0349cc9a60a170f9d80f7512e11c5
Manoj Naik <mnaik@juniper.net>
2016-02-11 14:58:03 +0530

    Closes-Bug: #1547769 - Added the flowrate bars to the vrouter bandwidth chart in Vrouter Details page. Also fixed the Tor Agent introspect port. Changed the labels in the Vrouter charts
    Change-Id: I3f64425ad3dd04a003ac9216354b239ece20461f

--- webroot/monitor/infrastructure/common/ui/js/utils/monitor.infra.utils.js
+++ webroot/monitor/infrastructure/common/ui/js/utils/monitor.infra.utils.js
@@ -1942,7 +1942,6 @@ define([
                     page:1,
 //                    timeRange:600,
                     tgUnits:'secs',
-                    tgValue:'60',
                     fromTimeUTC:'now-2h',
                     toTimeUTC:'now',
                     async:true,
@@ -1971,7 +1970,13 @@ define([
                         postData['select'] = 'Source, name, T, cpu_info.one_min_cpuload, cpu_info.used_sys_mem';
                     } else if (moduleType == 'vRouterBandwidth') {
                         postData['table'] = 'StatTable.VrouterStatsAgent.phy_if_band';
-                        postData['select'] = 'Source, name, T, phy_if_band.in_bandwidth_usage, phy_if_band.out_bandwidth_usage';
+                        postData['select'] = 'Source, name, T=, phy_if_band.in_bandwidth_usage, phy_if_band.out_bandwidth_usage';
+                        postData['tgValue'] = 60;
+                    } else if (moduleType == 'vRouterFlowRate') {
+                        postData['table'] = 'StatTable.VrouterStatsAgent.flow_rate';
+                        postData['select'] = 'Source, name, T=, MAX(flow_rate.active_flows)';
+                        postData['tgValue'] = 60;
+                        postData['plotFields'] = ['MAX(flow_rate.active_flows)'];
                     }
                     postData['where'] = '(Source = '+ node +' OR name = '+ node +')';
                 } else {
