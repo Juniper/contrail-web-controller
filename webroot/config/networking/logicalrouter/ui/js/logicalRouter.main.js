d70f3dc2c96be55d780591f2e5527b4566dd4b72
balamurugang <gbala@juniper.net>
2016-03-22 16:42:31 +0530

    Bug ID - 1440150 UI changes for user specified route-target for logical-router
    The UI support for adding the route target is done.
    
    Change-Id: I682aea5dce15beaa82893ffd44522cf3b1a231a2
    Closes-Bug: #1440150

--- webroot/config/networking/logicalrouter/ui/js/logicalRouter.main.js
+++ webroot/config/networking/logicalrouter/ui/js/logicalRouter.main.js
@@ -2,48 +2,32 @@
  * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
  */
 
-var ConfigLogicalRouterLoader = new ConfigLogicalRouterLoader();
+var configLogicalRouterLoader = new ConfigLogicalRouterLoader();
 
 function ConfigLogicalRouterLoader() {
     this.load = function (paramObject) {
         var self = this, currMenuObj = globalObj.currMenuObj,
             hashParams = paramObject['hashParams'],
             rootDir = currMenuObj['resources']['resource'][1]['rootDir'],
-            pathMNView = rootDir + '/js/views/logicalRouterView.js',
+            pathLogicalRouterView = ctBaseDir + '/config/networking/logicalrouter/ui/js/views/logicalRouterView.js',
             renderFn = paramObject['function'];
+            loadingStartedDefObj = paramObject['loadingStartedDefObj'];
 
-        check4CTInit(function () {
-            if (self.mnView == null) {
-                requirejs([pathMNView], function (LogicalRouterViewParam) {
-                    self.mnView = new LogicalRouterViewParam();
-                    self.renderView(renderFn, hashParams);
-                });
-            } else {
-                self.renderView(renderFn, hashParams);
+        require([pathLogicalRouterView], function (LogicalRouterView) {
+            self.logicalRouterView = new LogicalRouterView();
+            self.renderView(renderFn, hashParams);
+            if(contrail.checkIfExist(loadingStartedDefObj)) {
+                loadingStartedDefObj.resolve();
             }
         });
     }
     this.renderView = function (renderFn, hashParams) {
         $(contentContainer).html("");
-        this.mnView[renderFn]({hashParams: hashParams});
+        this.logicalRouterView[renderFn]({hashParams: hashParams});
     };
 
     this.updateViewByHash = function (hashObj, lastHashObj) {
         var renderFn;
         this.load({hashParams: hashObj, 'function': renderFn});
     };
-}
-function check4CTInit(callback) {
-    if (!ctInitComplete) {
-        requirejs(['controller-init'], function () {
-            ctInitComplete = true;
-            if(callback != null) {
-                callback()
-            }
-        });
-    } else {
-        if(callback != null) {
-            callback();
-        }
-    }
 }
