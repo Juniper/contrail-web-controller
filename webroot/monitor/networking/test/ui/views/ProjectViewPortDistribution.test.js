62a9ea135fef096ef4aff559ed95ab5f22b8b571
Sarin Kizhakkepurayil <sarink@juniper.net>
2016-04-08 10:21:23 -0700

    Closes-Bug: #1567603 separte test pages for each tab
    - moved test pages for dashboard view, instance view, network view, project view
    to separate test pages for each of their tabs.
    - updated Gruntfile.
    
    Change-Id: I874fbe980188c633074b55e052d1803df69f2481

--- webroot/monitor/networking/test/ui/views/ProjectViewPortDistribution.test.js
+++ webroot/monitor/networking/test/ui/views/ProjectViewPortDistribution.test.js
@@ -1,49 +1,41 @@
 /*
- * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
  */
 define([
     'co-test-runner',
     'ct-test-utils',
     'ct-test-messages',
-    'monitor/networking/test/ui/views/DashboardView.mock.data',
-    'co-tabs-view-test-suite'
-], function (cotr, cttu, cttm, TestMockdata, TabsViewTestSuite) {
+    'monitor/networking/test/ui/views/ProjectView.mock.data',
+    'co-chart-view-zoom-scatter-test-suite'
+], function (cotr, cttu, cttm, TestMockdata, ZoomScatterChartViewTestSuite) {
 
-    var moduleId = cttm.PROJECTS_LIST_VIEW_COMMON_TEST_MODULE;
+    var moduleId = cttm.PROJECTS_VIEW_COMMON_TEST_MODULE;
 
     var testType = cotc.VIEW_TEST;
 
     var fakeServerConfig = cotr.getDefaultFakeServerConfig();
 
-    var fakeServerResponsesConfig = function() {
+    var fakeServerResponsesConfig = function () {
         var responses = [];
 
-        /*
-         /api/tenants/config/domains                      done
-         /api/tenants/config/projects                     done
-         /api/tenant/networking/virtual-networks/details  done
-         /api/tenant/networking/network/stats/top?minsSince=10&fqName=default-domain:admin&useServerTime=true&type=port&_=1442526971361  done
-         /api/tenant/monitoring/project-connected-graph?fqName=default-domain:admin&_=1442526971867 done
-         /api/tenant/monitoring/project-config-graph?fqName=default-domain:admin&_=1442869670788 done
-         /api/tenant/networking/virtual-networks/details?count=25&fqn=default-domain:admin&startAt=1442869670641 done
-         /api/tenant/networking/virtual-machines/details?fqnUUID=ba710bf3-922d-4cda-bbb4-a2e2e76533bf&count=10&type=project  done
-         /api/tenant/networking/virtual-machine-interfaces/summary  done
-         /api/tenant/networking/stats done
-         */
-
-        responses.push(cotr.createFakeServerResponse( {
+        responses.push(cotr.createFakeServerResponse({
             url: cttu.getRegExForUrl(ctwc.URL_ALL_DOMAINS),
             body: JSON.stringify(TestMockdata.domainsMockData)
         }));
 
-        responses.push(cotr.createFakeServerResponse( {
+        responses.push(cotr.createFakeServerResponse({
             url: cttu.getRegExForUrl(ctwc.URL_ALL_PROJECTS),
             body: JSON.stringify(TestMockdata.projectMockData)
         }));
 
-        responses.push(cotr.createFakeServerResponse( {
+        responses.push(cotr.createFakeServerResponse({
             url: /\/api\/tenants\/projects\/default-domain.*$/,
             body: JSON.stringify(TestMockdata.projectMockData)
+        }));
+
+        responses.push(cotr.createFakeServerResponse({
+            url: /\/api\/tenants\/get-project-role.*$/,
+            body: JSON.stringify(null)
         }));
 
         responses.push(cotr.createFakeServerResponse({
@@ -63,30 +55,27 @@ define([
             url: cttu.getRegExForUrl('/api/tenant/monitoring/project-config-graph'),
             body: JSON.stringify(TestMockdata.projectConfigGraph)
         }));
+        
         responses.push(cotr.createFakeServerResponse({
-            method:"POST",
+            method: "POST",
             url: cttu.getRegExForUrl('/api/tenant/networking/virtual-networks/details'),
             body: JSON.stringify(TestMockdata.networksMockData)
         }));
+        
         responses.push(cotr.createFakeServerResponse({
-            method:"POST",
+            method: "POST",
             url: cttu.getRegExForUrl('/api/tenant/networking/virtual-machines/details'),
             body: JSON.stringify(TestMockdata.virtualMachinesDetailsMockData)
         }));
+        
         responses.push(cotr.createFakeServerResponse({
-            method:"POST",
+            method: "POST",
             url: cttu.getRegExForUrl('/api/tenant/networking/stats'),
             body: JSON.stringify(TestMockdata.networksMockStatData)
         }));
-        // how to differentiate between this POST request and the one for networks above
 
-        //responses.push(cotr.createFakeServerResponse({
-        //    method:"POST",
-        //    url: cttu.getRegExForUrl('/api/tenant/networking/stats'),
-        //    body: JSON.stringify(TestMockdata.virtualMachinesStatsMockData)
-        //}));
         responses.push(cotr.createFakeServerResponse({
-            method:"POST",
+            method: "POST",
             url: cttu.getRegExForUrl('/api/tenant/networking/virtual-machine-interfaces/summary'),
             body: JSON.stringify(TestMockdata.virtualMachinesSummaryMockData)
         }));
@@ -97,47 +86,58 @@ define([
 
     var pageConfig = cotr.getDefaultPageConfig();
     pageConfig.hashParams = {
-        p: 'mon_networking_dashboard',
+        p: 'mon_networking_projects',
         q: {
             view: 'details',
-            type: 'project'
+            type: 'project',
+            "focusedElement": {
+                "fqName": "default-domain:admin",
+                "type": "project"
+            },
+            "tab": {
+                "project-tabs": "project-ports-scatter-chart"
+            },
+            "reload": "false"
         }
     };
+
     pageConfig.loadTimeout = cotc.PAGE_LOAD_TIMEOUT * 5;
 
-    /**
-     * Test cases for components in each project tab will be tested in their respective tab pages.
-     */
-    var getTestConfig = function() {
+    var getTestConfig = function () {
         return {
             rootView: mnPageLoader.mnView,
             tests: [
                 {
-                    viewId: ctwl.PROJECT_TABS_ID,
+                    viewId: ctwl.PROJECT_PORTS_SCATTER_CHART_ID,
                     suites: [
                         {
-                            class: TabsViewTestSuite,
+                            class: ZoomScatterChartViewTestSuite,
                             groups: ['all']
                         }
                     ]
                 }
             ]
-        } ;
+        };
 
     };
 
-    var testInitFn = function (defObj, onAllViewsRenderComplete) {
-        setTimeout(function () {
-                onAllViewsRenderComplete.notify();
-                defObj.resolve();
-            },
-            // Add necessary timeout for the tab elements to load properly and resolve the promise
-            cotc.PAGE_INIT_TIMEOUT * 10
-        );
+    /**
+     * testInitFn is not truly required here as tab will be activated via URL on page load.
+     * implementing this function can add some timeout till the QUnit can be fired.
+     * manually notify the renderComplete event as rootView will not trigger one in this case as all views
+     * are already rendered.
+     * @param defObj
+     * @param renderComplete
+     */
+    var testInitFn = function(defObj, renderComplete) {
+        setTimeout(function() {
+            renderComplete.notify();
+            defObj.resolve();
+        }, cotc.PAGE_INIT_TIMEOUT * 10);
 
         return;
     };
-    
+
     var pageTestConfig = cotr.createPageTestConfig(moduleId, testType, fakeServerConfig, pageConfig, getTestConfig, testInitFn);
 
     cotr.startTestRunner(pageTestConfig);
