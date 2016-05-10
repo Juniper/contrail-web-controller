c997f7d34ffd843e4d74aab1416ccea0a985e7df
Siva Bavanasi <kbsiva@juniper.net>
2016-05-10 14:23:07 +0530

    Closes-bug: #1564678 - In Vcenter UI static ip address flag not shown in VN display in vcenter UI
    Change-Id: Ie2eb8944a18918e511b4541cb7eaa25248862336
    (cherry picked from commit cfbd86b762c4e8f711226e2b563aef331fb26822)

--- webroot/config/networking/networks/ui/js/views/vnCfgGridView.js
+++ webroot/config/networking/networks/ui/js/views/vnCfgGridView.js
@@ -274,6 +274,14 @@ define([
                                                     templateGenerator: 'TextGenerator'
                                                 },
                                                 {
+                                                    label: 'Static IP Addressing',
+                                                    key: 'external_ipam',
+                                                    templateGeneratorConfig: {
+                                                        formatter: 'staticIPAddressingFormatter'
+                                                    },
+                                                    templateGenerator: 'TextGenerator'
+                                                },
+                                                {
                                                     label: 'Shared',
                                                     key: 'uuid',
                                                     templateGenerator: 'TextGenerator',
@@ -450,6 +458,12 @@ define([
         return formatVNCfg.adminStateFormatter(null,
                                         null, null, null, dc);
     }
+
+    this.staticIPAddressingFormatter = function (v, dc) {
+        return formatVNCfg.staticIPAddressingFormatter(null,
+                                        null, null, null, dc);
+    };
+
     this.sharedFormatter = function (v, dc) {
         return formatVNCfg.sharedFormatter(null,
                                         null, null, null, dc);
