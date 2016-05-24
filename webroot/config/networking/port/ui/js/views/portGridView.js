87d2fb14dd04bd1d4c5ef7c18199aca06c3a7388
Siva Kumar Bavanasi <kbsiva@juniper.net>
2016-05-24 12:10:01 +0530

    Closes-bug: #1584994 - vcenter UI not displayed the ports
    Change-Id: I0da5d1b3970dad65dd825f8bb45eaa56af6243c3

--- webroot/config/networking/port/ui/js/views/portGridView.js
+++ webroot/config/networking/port/ui/js/views/portGridView.js
@@ -454,8 +454,8 @@ define([
                                         formatter: "localPrefFormater"
                                     }
                                 }, {
-                                    key: 'id_perms',
-                                    name:"id_perms",
+                                    key: 'ecmp_hashing_include_fields',
+                                    name:"ecmp_hashing_include_fields",
                                     label:"ECMP Hashing Fields",
                                     templateGenerator: 'TextGenerator',
                                     templateGeneratorConfig:{
@@ -545,7 +545,15 @@ define([
                     }
             });
         } else {
-            return({});
+            //return dummy key to hide device owner info
+            return({
+                key: 'dummy_vcenter',
+                label:"Device",
+                templateGenerator: 'TextGenerator',
+                templateGeneratorConfig:{
+                    formatter: "deviceOwnerFormatter"
+                }
+            });
         }
     }
     this.deviceOwnerUUID = function() {
@@ -560,7 +568,15 @@ define([
                     }
             });
         } else {
-            return({});
+          //return dummy key to hide device owner info
+            return({
+                key: 'dummy_vcenter',
+                label:"Device ID",
+                templateGenerator: 'TextGenerator',
+                templateGeneratorConfig:{
+                    formatter: "deviceUUIDFormatter"
+                }
+            });
         }
     }
     function subscribeModelChangeEvents(portModel, mode) {
