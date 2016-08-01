2acad9f68ed9ca0b47ad07f1d212865043a09d11
Siva Kumar Bavanasi <kbsiva@juniper.net>
2016-08-01 16:58:54 +0530

    Closes-bug: #1608492 - QoS dscp and other bits should be saved in API server as integer instead of binary bits
    Change-Id: I3ae3a41b68af3fa02f989690dc28d885b6c07dea

--- webroot/config/infra/globalconfig/ui/js/globalConfig.utils.js
+++ webroot/config/infra/globalconfig/ui/js/globalConfig.utils.js
@@ -49,23 +49,31 @@ define([
         },
 
         this.getTextByValue = function(arry, val) {
-            var i, arryCnt = arry.length, text = val;
+            var i, arryCnt = arry.length, text = null;
             for(i = 0; i < arryCnt; i++) {
                 if(arry[i].value == val){
                     text = arry[i].text;
                     break;
                 }
             }
+            if(text === null) {
+                //converts integer to binary
+                text = val.toString(2);
+            }
             return text;
         },
 
         this.getValueByText =  function(arry, txt) {
-            var i, arryCnt = arry.length, value = txt;
+            var i, arryCnt = arry.length, value = null;
             for(i = 0; i < arryCnt; i++) {
                 if(arry[i].text === txt){
                     value = arry[i].value;
                     break;
                 }
+            }
+            if(value === null) {
+                //converts binary to integer
+                value = parseInt(txt, 2);
             }
             return value;
         }
