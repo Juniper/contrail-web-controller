3e07b93491631e3d74f6e7a54d5c494776f491f7
Manoj Naik <mnaik@juniper.net>
2016-08-06 16:33:52 +0530

    Closes-Bug: #1609563 - Fixed the issue where alarms were not being reported in the scatter chart or the grid for config and database node.
    Change-Id: I51c11acf785122a0463a8dfcd5fdbaccf2096761

--- webroot/monitor/infrastructure/common/ui/js/utils/monitor.infra.parsers.js
+++ webroot/monitor/infrastructure/common/ui/js/utils/monitor.infra.parsers.js
@@ -562,7 +562,7 @@ define(
                         obj['isGeneratorRetrieved'] = false;
                         obj['nodeAlerts'] =
                             infraMonitorAlertUtils.processConfigNodeAlerts(obj);
-                        var alarms = getValueByJsonPath(d,'value;configNode;UVEAlarms;alarms',[]);
+                        var alarms = getValueByJsonPath(d,'value;UVEAlarms;alarms',[]);
                         if(cowu.getAlarmsFromAnalytics) {
                             obj['alerts'] = coreAlarmUtils.getAlertsFromAnalytics(
                                                             {
@@ -669,7 +669,7 @@ define(
                             monitorInfraUtils.isNTPUnsynced(jsonPath(d,'$..NodeStatus')[0]);
                         obj['nodeAlerts'] =
                             infraMonitorAlertUtils.processDbNodeAlerts(obj);
-                        var alarms = getValueByJsonPath(d,'value;databaseNode;UVEAlarms;alarms',[]);
+                        var alarms = getValueByJsonPath(d,'value;UVEAlarms;alarms',[]);
                         if(cowu.getAlarmsFromAnalytics) {
                             obj['alerts'] = coreAlarmUtils.getAlertsFromAnalytics(
                                                             {
