2fa5685350670b53cbb5db37803d1a7ce7b95940
Siva Kumar Bavanasi <kbsiva@juniper.net>
2016-08-19 17:27:25 +0530

    Changes for hiding Monitor alarms page and  Physical Devices and RBAC Config pages for non-admin user
    and also commented call to get is_shared vns in service instance
    
    Change-Id: I67033f71af6e98ee39ead5a32a4144c8aae37afc
    Related-bug: #1594301

--- webroot/config/services/instances/ui/js/views/svcInstEditView.js
+++ webroot/config/services/instances/ui/js/views/svcInstEditView.js
@@ -145,11 +145,11 @@ define([
                     },
                     {
                         'type': 'route-aggregates'
-                    },
+                    }/*,
                     {
                         'type': 'virtual-networks',
                         'filters': 'is_shared==true'
-                    }]}
+                    }*/]}
                 });
             if ((null != setVNList) && (setVNList.length > 0)) {
                 multArrFlag = true;
