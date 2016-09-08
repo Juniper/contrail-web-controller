889cea43d62ab44d377e79422b9e801b03a16020
Akhil Francis <akhilf@juniper.net>
2016-09-08 15:19:05 +0530

    Closes-bug:#1621398 - 95th Percentile for Analytics Node chart
    Change-Id: I2c5f0388e8aa283123528452b5fc69414509a529

---
+++ webroot/monitor/infrastructure/analyticsnode/ui/js/views/PercentileTextView.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define(['underscore', 'contrail-view', 'contrail-list-model'],
+       function(_, ContrailView,ContrailListModel){
+        var PercentileTextView = ContrailView.extend({
+        render : function (){
+            var percentileTextViewTemplate = contrail.getTemplate4Id(
+                    cowc.PERCENTILE_TEXT_VIEW_TEMPLATE);
+            var viewConfig = this.attributes.viewConfig;
+            var self = this;
+            if (self.model === null && viewConfig['modelConfig'] != null) {
+                self.model = new ContrailListModel(viewConfig['modelConfig']);
+            }
+            if (self.model !== null) {
+                self.model.onDataUpdate.subscribe(function () {
+                    self.renderTemplate($(self.$el), viewConfig, self.model,percentileTextViewTemplate);
+                });
+                self.model.onAllRequestsComplete.subscribe(function () {
+                    self.renderTemplate($(self.$el), viewConfig, self.model,percentileTextViewTemplate);
+                });
+             }
+           },
+        renderTemplate: function (selector, viewConfig, chartViewModel,percentileTextViewTemplate) {
+            var chartModelItems = chartViewModel.getItems(), self = this;
+            if(chartModelItems){
+            self.$el.html(percentileTextViewTemplate({percentileMessagesobjVal: chartModelItems[0].percentileMessagesobjVal ,percentileSizeobjVal: chartModelItems[0].percentileSizeobjVal}));
+              }
+           }
+    });
+
+   return PercentileTextView;
+});
