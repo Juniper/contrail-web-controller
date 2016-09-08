734e9244ad514092b88cd4c37a67dd67df1f8775
Akhil Francis <akhilf@juniper.net>
2016-09-08 15:19:05 +0530

    Closes-Bug:#1621398 - 95th Percentile for Analytics Node and DB node chart
    Change-Id: I189c594c915e17560b35da79fae260bba996cecc

--- webroot/monitor/infrastructure/common/ui/js/views/AnalyticsNodesSummaryChartsView.js
+++ webroot/monitor/infrastructure/common/ui/js/views/AnalyticsNodesSummaryChartsView.js
@@ -14,14 +14,18 @@ define(['underscore', 'contrail-view',
         render : function (){
             var anlyticsTemplate = contrail.getTemplate4Id(
                     cowc.TMPL_4COLUMN__2ROW_CONTENT_VIEW);
+            var percentileWrapperTemplate = contrail.getTemplate4Id(
+                    cowc.TMPL_1COLUMN__1ROW_CONTENT_VIEW);
             var self = this,
                 viewConfig = self.attributes.viewConfig,
                 colorFn = viewConfig['colorFn'];
-            self.$el.html(anlyticsTemplate);
+            self.$el.append(anlyticsTemplate);
+            self.$el.append(percentileWrapperTemplate({cssClass: 'percentileWrapper col-xs-6 col-xs-offset-6'}));
             var topleftColumn = self.$el.find(".top-container .left-column"),
                 toprightCoulmn = self.$el.find(".top-container .right-column"),
                 bottomleftColumn = self.$el.find(".bottom-container .left-column"),
                 bottomrightCoulmn = self.$el.find(".bottom-container .right-column"),
+                bottomRow = self.$el.find(".percentileWrapper"),
                 sandeshModel = new AnalyticsNodeSandeshChartModel(),
                 queriesModel = new AnalyticsNodeQueriesChartModel(),
                 dbUsageModel = new AanlyticsNodeDatabaseUsageModel();
@@ -37,6 +41,8 @@ define(['underscore', 'contrail-view',
 
             self.renderView4Config(bottomleftColumn,  databseReadWritemodel,
                     getAnalyticsNodeDatabaseWriteChartViewConfig(colorFn));
+
+            self.renderView4Config(bottomRow, null,getPercentileTextViewConfig());
         }
     });
    function getAnalyticsNodeSandeshChartViewConfig(colorFn) {
@@ -195,6 +201,58 @@ define(['underscore', 'contrail-view',
                                colors: colorFn,
                                showControls: false,
                            },
+                       }
+                   }]
+               }]
+           }
+       }
+
+   }
+   function getPercentileTextViewConfig() {
+       var queryPostData = {
+           "autoSort": true,
+           "async": false,
+           "formModelAttrs": {
+            "table_name": "StatTable.SandeshMessageStat.msg_info",
+             "table_type": "STAT",
+             "query_prefix": "stat",
+             "from_time": Date.now() - (2 * 60 * 60 * 1000),
+             "from_time_utc": Date.now() - (2 * 60 * 60 * 1000),
+             "to_time": Date.now(),
+             "to_time_utc": Date.now(),
+             "select": "PERCENTILES(msg_info.bytes), PERCENTILES(msg_info.messages)",
+             "time_granularity": 30,
+             "time_granularity_unit": "mins",
+             "limit": "150000"
+           },
+       };
+       return {
+           elementId : ctwl.ANALYTICS_CHART_PERCENTILE_SECTION_ID,
+           view : "SectionView",
+           viewConfig : {
+               rows : [ {
+                   columns : [ {
+                       elementId :ctwl.ANALYTICS_CHART_PERCENTILE_TEXT_VIEW,
+                       title : '',
+                       view : "PercentileTextView",
+                       viewPathPrefix:
+                           ctwl.ANALYTICSNODE_VIEWPATH_PREFIX,
+                       app : cowc.APP_CONTRAIL_CONTROLLER,
+                       viewConfig : {
+                              modelConfig : {
+                                   remote : {
+                                       ajaxConfig : {
+                                           url : "/api/qe/query",
+                                           type: 'POST',
+                                           data: JSON.stringify(queryPostData),
+                                           timeout : 120000 //2 mins
+                                       },
+                                       dataParser : function (response) {
+                                           return monitorInfraParsers.percentileAnalyticsNodeSummaryChart(response['data']);
+                                       }
+                                   },
+                                   cacheConfig : {}
+                               },
                        }
                    }]
                }]
