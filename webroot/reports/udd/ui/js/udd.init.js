4cda3b88a9246f0e468c7fd9ee3dfaeda760830e
Zheyang Song <zsong@juniper.net>
2016-10-31 13:18:35 -0700

    UDD Enhancement for R3.2 - Patch-up
    * Regulate usage of RequireJS module path.
    
    Change-Id: I54b01f301f0c64da465332a938121c9c5559eb1a
    Related-Bug: #1637320

--- webroot/reports/udd/ui/js/udd.init.js
+++ webroot/reports/udd/ui/js/udd.init.js
@@ -1,9 +1,9 @@
 /* Copyright (c) 2016 Juniper Networks, Inc. All rights reserved. */
 
 define([
-    "text!reports/qe/ui/templates/qe.tmpl",
-    "text!reports/udd/ui/templates/udd.tmpl",
-    "reports/udd/ui/js/udd.main"
+    "text!controller-basedir/reports/qe/ui/templates/qe.tmpl",
+    "text!core-basedir/reports/udd/ui/templates/udd.tmpl",
+    "controller-basedir/reports/udd/ui/js/udd.main"
 ], function (QETemplates, UDDTemplates) {
     var initJSpath = window.pkgBaseDir + "/reports/udd/ui/js/udd.init.js";
     var initStatus = window.contentHandler.initFeatureModuleMap[initJSpath];
