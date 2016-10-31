1484ddd9d10b0ac2fdcb1233fff3e6966b31e39d
Zheyang Song <zsong@juniper.net>
2016-10-31 13:18:35 -0700

    UDD Enhancement for R3.2 - Patch-up
    * Regulate usage of RequireJS module path.
    
    Change-Id: Ife6cccf8a34fdc8f49d8f1b0ce90b5a860111cd9
    Related-Bug: #1637320

--- webroot/reports/udd/ui/js/udd.main.js
+++ webroot/reports/udd/ui/js/udd.main.js
@@ -2,6 +2,6 @@
  * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
  */
 
-define(["reports/udd/ui/js/udd.bootstrap.js"], function(UDDashboardLoader) {
+define(["core-basedir/reports/udd/ui/js/udd.bootstrap"], function(UDDashboardLoader) {
     window.WCUDDLoader = new UDDashboardLoader();
 });
