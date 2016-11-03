a37c32de03d0907ee4011bebe485298ced8009a7
Sarin Kizhakkepurayil <sarink@juniper.net>
2016-11-03 13:51:11 -0700

    Fix UDD feature loading
    - controller.udd will use core udd module for page loading.
    - menu updated to reflect the loader change.
    - upadte to UT Gruntfile
    
    Change-Id: Ie9380a8eeb83288ce5941a580dc27bae026b42bc
    Closes-Bug: #1639045

--- webroot/reports/udd/ui/js/controller.udd.init.js
+++ webroot/reports/udd/ui/js/controller.udd.init.js
@@ -1,19 +1,14 @@
 /* Copyright (c) 2016 Juniper Networks, Inc. All rights reserved. */
 
 define([
-    "text!controller-basedir/reports/qe/ui/templates/qe.tmpl",
-    "text!core-basedir/reports/udd/ui/templates/udd.tmpl",
-    "controller-basedir/reports/udd/ui/js/udd.main"
-], function (QETemplates, UDDTemplates) {
-    var initJSpath = window.pkgBaseDir + "/reports/udd/ui/js/udd.init.js";
+    "udd-module"
+], function () {
+    var initJSpath = window.pkgBaseDir + "/reports/udd/ui/js/controller.udd.init.js";
     var initStatus = window.contentHandler.initFeatureModuleMap[initJSpath];
     var deferredObj = initStatus.deferredObj;
 
     initStatus.isInProgress = false;
     initStatus.isComplete = true;
-
-    $("body").append(QETemplates);
-    $("body").append(UDDTemplates);
 
     if (window.contrail.checkIfExist(deferredObj)) {
         deferredObj.resolve();
