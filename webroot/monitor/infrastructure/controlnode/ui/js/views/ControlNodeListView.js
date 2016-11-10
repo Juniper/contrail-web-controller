6c782085826bf26b356f16ba8fcd1c8487f72ed9
Akhil Francis <akhilf@juniper.net>
2016-11-10 15:50:55 +0530

    Related-Bug:#1633013 - Added the new charts System memory , System Usage , Geneerator Scatter chart etc                        Modified the labels for the charts and order                        Modified the code as per the serilization code pattern
    Change-Id: I257ea2626b68d3df9fc74f4fba722b1fa722d682

--- webroot/monitor/infrastructure/controlnode/ui/js/views/ControlNodeListView.js
+++ webroot/monitor/infrastructure/controlnode/ui/js/views/ControlNodeListView.js
@@ -3,9 +3,9 @@
  */
 
 define(
-        [ 'underscore', 'contrail-view','node-color-mapping'],
+        [ 'underscore', 'contrail-view', 'node-color-mapping'],
         function(
-                _, ContrailView,NodeColorMapping) {
+                _, ContrailView, NodeColorMapping) {
             var ControlNodeListView = ContrailView.extend({
                 render : function() {
                     var nodeColorMapping = new NodeColorMapping(),
@@ -16,54 +16,80 @@ define(
             });
             function getControlNodeListViewConfig(colorFn) {
                 var viewConfig = {
-                    rows : [{
-                        columns : [{
-                            elementId: 'control-node-carousel-view',
-                            view: "CarouselView",
-                            viewConfig: {
-                            pages : [
-                                     {
-                                         page: {
-                                             elementId : 'control-node-grid-stackview-0',
+                    rows : [
+                        {
+                            columns : [{
+                                elementId: 'control-node-carousel-view',
+                                view: "CarouselView",
+                                viewConfig: {
+                                pages : [
+                                         {
+                                             page: {
+                                                 elementId : 'control-node-grid-stackview-0',
+                                                 view : "GridStackView",
+                                                 viewConfig: {
+                                                     elementId : 'control-node-grid-stackview-0',
+                                                     gridAttr : {
+                                                         defaultWidth : 6,
+                                                         defaultHeight : 8
+                                                     },
+                                                     widgetCfgList: [
+                                                         {id:'controlnode-sent-updates'},
+                                                         {id:'controlnode-received-updates'},
+                                                         {id:'controlnode-control'},
+                                                         {id:'controlnode-memory'},
+                                                         {id:'controlnode-grid-view'}
+                                                     ]
+                                                  }
+                                               }
+                                         },{
+                                             page: {
+                                                 elementId : 'control-node-grid-stackview-1',
+                                                 view : "GridStackView",
+                                                 viewConfig: {
+                                                     elementId : 'control-node-grid-stackview-1',
+                                                     gridAttr : {
+                                                         defaultWidth : 6,
+                                                         defaultHeight : 8
+                                                     },
+                                                     widgetCfgList: [
+                                                         {id:'controlnode-dns'},
+                                                         {id:'controlnode-named'},
+                                                         {id:'system-cpu-share'},
+                                                         {id:'system-memory-usage'},
+                                                        // {id:'disk-usage-info'},
+                                                         {id:'controlnode-grid-view'}
+                                                     ]
+                                                }
+                                             },
+                                         },{
+                                             page: {
+                                             elementId : 'control-node-grid-stackview-2',
                                              view : "GridStackView",
                                              viewConfig: {
-                                                elementId : 'control-node-grid-stackview-0',
-                                                gridAttr : {
-                                                    defaultWidth : 6,
-                                                    defaultHeight : 8
-                                                },
-                                                widgetCfgList: [
-                                                    {
-                                                        id: 'controlnode-sent-updates',
-                                                    },
-                                                    {   
-                                                        id: 'controlnode-received-updates',
-                                                    },
-                                                    {   
-                                                        id: 'controlnode-cpu-share',
-                                                    },
-                                                    {   
-                                                        id: 'controlnode-memory',
-                                                    },
-                                                    {   
-                                                        id: 'controlnode-grid-view',
-                                                    }
-                                                    // 'controlnode-system-logs'
-                                                ]
-                                             }
+                                                 elementId : 'control-node-grid-stackview-2',
+                                                 gridAttr : {
+                                                     defaultWidth : 6,
+                                                     defaultHeight : 8
+                                                 },
+                                                 widgetCfgList: [
+                                                     {id:'disk-usage-info'},
+                                                     {id:'controlnode-grid-view'}
+                                                 ]
+                                            }
                                          },
-                                     }
-                               ]
-                            }
+                                      }
+                                   ]
+                                }
+                            }]
                         }]
-                    }]
                 };
                 return {
                     elementId : cowu.formatElementId([
-                         ctwl.CONTROLNODE_SUMMARY_LIST_SECTION_ID ]),
+                          ctwl.CONTROLNODE_SUMMARY_LIST_SECTION_ID ]),
                     view : "SectionView",
-                    viewConfig :viewConfig
+                    viewConfig : viewConfig
                 };
             }
-        return ControlNodeListView;
-    });
+            return ControlNodeListView;
+        });
