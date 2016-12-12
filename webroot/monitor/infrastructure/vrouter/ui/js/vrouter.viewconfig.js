57a54fe0bdd6c9a6bb0942cdfb0e8b14d45663fa
Manoj Naik <mnaik@juniper.net>
2016-12-12 21:46:45 +0530

    Closes-Bug: #1648695 - Changed the query for retrieving drop_stats for vRouters. Also changed to use the default chartDataFormatter parser instead of parseDataForLineChart
    Change-Id: Ib79e365b5d6710b1e9ad83a8a8fffaa135a53571

--- webroot/monitor/infrastructure/vrouter/ui/js/vrouter.viewconfig.js
+++ webroot/monitor/infrastructure/vrouter/ui/js/vrouter.viewconfig.js
@@ -101,7 +101,6 @@ define(['underscore', 'contrail-view','contrail-list-model', 'cf-datasource', 'l
                          elementId : 'flow_rate_area_chart',
                          view: 'LineWithFocusChartView',
                          viewConfig: {
-                             parseFn: cowu.parseDataForLineChart,
                              chartOptions: {
                                  title: ctwl.VROUTER_ACTIVE_FLOWS_DROPS_LABEL,
                                  subTitle:"Sampled Active Flows across vRouters",
@@ -111,9 +110,9 @@ define(['underscore', 'contrail-view','contrail-list-model', 'cf-datasource', 'l
                                  yFormatter: function(y) {
                                      return _.isNaN(y)? y : parseInt(y);
                                  },
+                                 groupBy:null,
                                  yAxisLabel: ctwl.VROUTER_ACTIVE_FLOWS_DROPS_LABEL,
-                                 yLabels: ['Active Flows'],
-                                 yFields: ['MAX(flow_rate.active_flows)'],
+                                 yField: 'MAX(flow_rate.active_flows)'
                              }
                          }
                     },
@@ -162,16 +161,13 @@ define(['underscore', 'contrail-view','contrail-list-model', 'cf-datasource', 'l
                          source:"STATTABLE",
                          config: {
                              table_name: 'StatTable.VrouterStatsAgent.drop_stats',
-                             select: 'T=, Source, MAX(drop_stats.__value)',
-                             parser: function(d){return parseDataForFlowsDrops(d,'MAX(drop_stats.__value)')},
-                             where: getWhereClauseForDropStats(),
+                             select: 'T=, SUM(drop_stats.drop_pkts)'
                          }
                      },
                      viewCfg: {
                           elementId : 'drop_packets_chart',
                           view: 'LineWithFocusChartView',
                           viewConfig: {
-                              parseFn: cowu.parseDataForLineChart,
                               chartOptions: {
                                   title: ctwl.VROUTER_DROP_PACKETS,
                                   subTitle:" Packet Drops across vRouters",
@@ -181,9 +177,9 @@ define(['underscore', 'contrail-view','contrail-list-model', 'cf-datasource', 'l
                                   yFormatter: function(y) {
                                       return _.isNaN(y)? y : parseInt(y);
                                   },
+                                  groupBy:null,
+                                  yField:'SUM(drop_stats.drop_pkts)',
                                   yAxisLabel: ctwl.VROUTER_DROP_PACKETS,
-                                  yLabels: ['Packet Drops'],
-                                  yFields: ['MAX(drop_stats.__value)'],
                               }
                           }
                      },
