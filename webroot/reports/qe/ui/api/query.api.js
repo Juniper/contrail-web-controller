76977fadd8b4e06d45c6bae44007eb4510e2e082
Zheyang Song <zsong@juniper.net>
2017-02-13 11:19:27 -0800

    Cross-Site Scripting (XSS) Contrail vulnerability
    + Add code to sanitize the value of "engQueryStr" posted to "/api/admin/reports/query"
    
    Change-Id: I661533b54d57d2bd4977b418090ac26a5df5ad6c
    Related-Bug: #1657280

--- webroot/reports/qe/ui/api/query.api.js
+++ webroot/reports/qe/ui/api/query.api.js
@@ -191,7 +191,7 @@ function runQuery(req, res, queryReqObj, appData) {
 
     cachedResultConfig = {"queryId": queryId, "chunk": chunk, "sort": sort, "chunkSize": chunkSize, "toSort": true};
 
-    logutils.logger.debug('Query Request: ' + JSON.stringify(queryReqObj));
+    logutils.logger.debug('Query Request: ' + JSON.stringify(queryReqObj, null, 4));
 
     if (queryId != null) {
         redisClient.exists(queryId + ':chunk1', function (err, exists) {
@@ -218,6 +218,17 @@ function runNewQuery(req, res, queryId, queryReqObj, appData) {
 };
 
 function getQueryOptions(queryReqObj) {
+    queryReqObj = commonUtils.sanitizeXSS(queryReqObj)
+    // queryReqObj["engQueryStr"] = JSON.stringify(_.object(_.map(
+    //     _.pairs(JSON.parse(queryReqObj["engQueryStr"])),
+    //     function(pair) {
+    //         pair[1] = _.escape(pair[1]);
+    //         return pair;
+    //     }
+    // )))
+    logutils.logger.debug("==========================");
+    logutils.logger.debug(JSON.stringify(queryReqObj, null, 2));
+    logutils.logger.debug("==========================");
     var formModelAttrs = queryReqObj['formModelAttrs'], tableType = formModelAttrs['table_type'],
         queryId = queryReqObj['queryId'], chunkSize = parseInt(queryReqObj['chunkSize']),
         async = (queryReqObj['async'] != null) ? queryReqObj['async'] : false;
