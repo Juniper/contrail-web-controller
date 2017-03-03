2ecd777a69035d9ee0ceda7b5b3d0f5314328f01
Manoj Naik <mnaik@juniper.net>
2017-03-03 11:31:00 +0530

    Related-Bug: #1657280 - Sanitizing the values for introspect page. For others its handled in the core.
    Change-Id: I7ca5ee30d51eff8cd2e1764b73f029788524008f

--- webroot/setting/introspect/ui/js/views/IntrospectJSGridView.js
+++ webroot/setting/introspect/ui/js/views/IntrospectJSGridView.js
@@ -94,6 +94,7 @@ define([
         }
 
         _.each(dataRecord, function (value, key) {
+            value = JSON.parse(cowu.sanitize(JSON.stringify(value)));
             if (key.charAt(0) !== "_" && ["more", "next_batch"].indexOf(key) === -1 ) {
                 var gridColumn = {
                     id: key, field: key,
@@ -105,7 +106,7 @@ define([
                 if (contrail.checkIfExist(value._link)) {
                     gridColumn.formatter = function (r, c, v, cd, dc) {
                         return '<a class="introspect-link" data-link="' + value._link + '" ' +
-                            'x="' + dc[key] + '">' + dc[key] + "</a>";
+                            'x="' + cowu.sanitize(dc[key]) + '">' + cowu.sanitize(dc[key]) + "</a>";
                     };
                     gridColumn.exportConfig = { allow: true };
 
