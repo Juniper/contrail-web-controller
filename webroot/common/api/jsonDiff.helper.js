8672ef491ff50a85a9da33a4b16550718f47e11f
Siva Bavanasi <kbsiva@juniper.net>
2017-03-08 17:43:53 +0530

    Fixed the issue with port update
    Below issues are addressed
    1)As we are removing 'attr = null' from ui in refs, always need to remove attr
    from refs in apiserver to get correct json diff
    2)use ref-update to update bridge_domans_refs on vmi
    
    Change-Id: I9b8136666c30bc434e9f7755678d3ccce7652482
    Related-bug: #1664476

--- webroot/common/api/jsonDiff.helper.js
+++ webroot/common/api/jsonDiff.helper.js
@@ -69,7 +69,7 @@ var configJsonModifyObj = {
     'virtual-machine-interface': {
         'isConfig': true,
         'preProcessCB': {
-            'applyOnOldJSON': modifyConfigDataByHrefUUID,
+            'applyOnOldJSON': modifyConfigDataByAttrHrefUUID,
             'applyOnNewJSON': modifyConfigDataByHrefUUID
         },
         'optFields': ['ecmp_hashing_include_fields', 'virtual_machine_interface_bindings',
@@ -84,7 +84,6 @@ var configJsonModifyObj = {
             'virtual_network_refs', 'virtual_machine_interface_device_owner',
             'virtual_machine_interface_disable_policy',
             'qos_config_refs',
-            'bridge_domain_refs',
             'port_security_enabled',
             'perms2'
         ],
