6082d867ec8fd525f5a76b1f71c69b2d7f95e49e
Rajiv Sah <rajivks@juniper.net>
2017-03-14 01:15:09 -0700

    Closes-Bug:#1671292-Gohan UI Changes
    Change-Id: Ie49eb383992b073b43bc99509ea8c461ea07e597

---
+++ webroot/config/gohanUi/ui/js/views/securitygroup/SecGrpView.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view'
+], function (_, ContrailView) {
+    var SecGrpView = ContrailView.extend({
+        el: $('#gohanGrid'),
+        renderSecGrp: function (viewConfig) {
+            var self = this;
+            $('#gohan-config-role').show();
+            self.renderView4Config(self.$el, null, getSecGrp(viewConfig));
+        }
+    });
+
+    function getSecGrp(viewConfig) {
+        return {
+                elementId: cowu.formatElementId([ctwl.CONFIG_QUOTAS_PAGE_ID]),
+                view: "SecGrpListView",
+                app: cowc.APP_CONTRAIL_CONTROLLER,
+                viewPathPrefix: "config/gohanUi/ui/js/views/securitygroup/",
+                viewConfig: viewConfig
+            }
+    };
+    return SecGrpView;
+});
+
