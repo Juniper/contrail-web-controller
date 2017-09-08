6acc97524093e70183f55a16efae9b443013bc51
rajivks <rajivks@juniper.net>
2017-09-08 19:31:21 +0530

    Need to add configuration for Security logging object
    Closes-Bug: #1657975
    
    Change-Id: I9030fb7f87b9ffd116cf3787fac7d4915545e377

---
+++ webroot/config/networking/slo/ui/js/views/sloListView.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view',
+    'contrail-list-model',
+], function (_, ContrailView, ContrailListModel) {
+    var self;
+    var sloListView = ContrailView.extend({
+        el: $(contentContainer),
+
+        render: function () {
+            self = this;
+            var listModelConfig, contrailListModel,
+                viewConfig = self.attributes.viewConfig,
+                currentProject = viewConfig["projectSelectedValueData"];
+
+            listModelConfig = {
+                remote: {
+                    ajaxConfig: {
+                        url: ctwc.URL_GET_CONFIG_DETAILS,
+                        type: "POST",
+                        data: JSON.stringify({data: [{type: "security-logging-objects",
+                                parent_id: currentProject.value}]})
+                    },
+                    dataParser: self.parseSloProjectData,
+                }
+            };
+            contrailListModel = new ContrailListModel(listModelConfig);
+            self.renderView4Config(self.$el,
+                    contrailListModel, self.getSloProjectGridViewConfig(viewConfig));
+        },
+
+        parseSloProjectData: function(result) {
+            var sloConfigDataSrc = [],
+            sloConfigs = getValueByJsonPath(result,
+                "0;security-logging-objects",
+                [], false);
+            _.each(sloConfigs, function(sloConfig) {
+                if("security-logging-object" in sloConfig) {
+                    sloConfigDataSrc.push(sloConfig["security-logging-object"])
+                }
+            });
+            return sloConfigDataSrc;
+        },
+
+        getSloProjectGridViewConfig: function(viewConfig) {
+            return {
+                elementId:
+                    cowu.formatElementId([ctwc.CONFIG_SLO_SECTION_ID]),
+                view: "SectionView",
+                viewConfig: {
+                    rows: [{
+                        columns: [
+                            {
+                                elementId: ctwc.CONFIG_SLO_ID,
+                                view: "sloGridView",
+                                viewPathPrefix:
+                                    "config/networking/slo/common/ui/js/views/",
+                                app: cowc.APP_CONTRAIL_CONTROLLER,
+                                viewConfig: {
+                                    isGlobal: false,
+                                    currentProject: viewConfig["projectSelectedValueData"]
+                                }
+                            }
+                        ]
+                    }]
+                }
+            }
+        }
+    });
+
+    return sloListView;
+});
+
