962276cba2c6bd967b9b521faa6b2104e30d1ae2
Siva Kumar Bavanasi <kbsiva@juniper.net>
2017-11-17 20:24:44 +0530

    Added Modal UT for BGP Routers
    Change-Id: Iaaa95a5b5211cfcaac80ae1a1d8f4a7a348f555f
    Partial-bug: #1732922

--- webroot/config/infra/bgp/test/ui/views/bgpGridView.test.js
+++ webroot/config/infra/bgp/test/ui/views/bgpGridView.test.js
@@ -8,8 +8,9 @@ define([
     'ct-test-messages',
     'config/infra/bgp/test/ui/views/bgpGridView.mock.data',
     'co-grid-contrail-list-model-test-suite',
-    'co-grid-view-test-suite'
-], function (cotc, cotr, cttu, cttm, TestMockdata, GridListModelTestSuite, GridViewTestSuite) {
+    'co-grid-view-test-suite',
+    'config/infra/bgp/test/ui/views/bgpModal.test.suite'
+], function (cotc, cotr, cttu, cttm, TestMockdata, GridListModelTestSuite, GridViewTestSuite, BGPModalTestSuite) {
 
     var moduleId = cttm.BGP_GRID_VIEW_COMMON_TEST_MODULE;
 
@@ -23,6 +24,24 @@ define([
         responses.push(cotr.createFakeServerResponse( {
             url: /\/api\/tenants\/config\/bgp\/get-bgp-routers.*$/,
             body: JSON.stringify(TestMockdata.bgpMockData)
+        }));
+        responses.push(cotr.createFakeServerResponse( {
+            url: /\/api\/tenants\/admin\/config\/global-asn.*$/,
+            body: JSON.stringify(TestMockdata.globalASNMockData)
+        }));
+        responses.push(cotr.createFakeServerResponse( {
+            url: /\/api\/tenants\/config\/physical-routers-list.*$/,
+            body: JSON.stringify(TestMockdata.prMockData)
+        }));
+        responses.push(cotr.createFakeServerResponse( {
+            url: /\/api\/tenants\/config\/get-config-details.*$/,
+            body: JSON.stringify(TestMockdata.tagsMockData)
+        }));
+        responses.push(cotr.createFakeServerResponse({
+            url: /\/api\/admin\/bgp-router.*$/,
+            method: 'POST',
+            postBody: JSON.stringify(TestMockdata.bgpModalMockOutput),
+            body: JSON.stringify(null)
         }));
 
         return responses;
@@ -45,7 +64,11 @@ define([
                         {
                             class: GridViewTestSuite,
                             groups: ['all']
-                        }
+                        }/*,
+                        {
+                            class: BGPModalTestSuite,
+                            groups: ['all']
+                        }*/
                     ]
                 }
             ]
@@ -54,7 +77,5 @@ define([
     };
 
     var pageTestConfig = cotr.createPageTestConfig(moduleId, testType, fakeServerConfig, pageConfig, getTestConfig);
-
-    cotr.startTestRunner(pageTestConfig);
-
+    cotr.startTestRunner(pageTestConfig)
 });
