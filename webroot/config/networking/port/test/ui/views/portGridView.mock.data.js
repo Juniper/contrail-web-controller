105e3a7dd0fe52cd37d0f2143dc3fc90f3a06460
Akhil <akhilf@juniper.net>
2018-01-18 12:47:51 +0530

    Incorporate schema changes for Fat Flow
    Added a checkbox in UI for each row(Port,Protocol) under "Flat Flows"
    and set it true or false based on user selection
    
    Allow user to enter zero also in Port
    
    Added the test case for port page popup with checkbox check
    
    Change-Id: Iea077d98fd4d7ec73da15f38a6b0fff164caafc6
    Closes-Bug: #1742431

--- webroot/config/networking/port/test/ui/views/portGridView.mock.data.js
+++ webroot/config/networking/port/test/ui/views/portGridView.mock.data.js
@@ -38,7 +38,10 @@
        "b5577a6e-0102-448e-af7b-89ca473e6f3a",
        "704ccb89-94d6-43ab-a245-c7b825b409dd"
      ];
-
+     this.portModalMockCNInput = {
+             "virtualNetworkName": "test_refs"
+     };
+     this.portModalMockCNOutput = {"virtual-machine-interface":{"fq_name":["default-domain","admin"],"parent_type":"project","id_perms":{"enable":true},"virtual_network_refs":[{"to":["default-domain","admin","test_refs"]}],"port_security_enabled":true,"security_group_refs":[{"to":["default-domain","admin","default"]}],"floating_ip_back_refs":[],"virtual_machine_interface_allowed_address_pairs":{},"virtual_machine_interface_device_owner":"","security_logging_object_refs":[],"service_health_check_refs":[],"instance_ip_back_refs":[{"instance_ip_address":[{"fixedIp":"","domain":"default-domain","project":"admin"}],"subnet_uuid":"26ca114a-5fb9-488a-892d-b76041530fff","instance_ip_family":"v4"}],"interface_route_table_refs":[],"virtual_machine_interface_dhcp_option_list":{},"virtual_machine_interface_fat_flow_protocols":{"fat_flow_protocol":[]},"virtual_machine_interface_bindings":{"key_value_pair":[]},"virtual_machine_interface_properties":{"local_preference":null,"interface_mirror":null},"virtual_machine_interface_refs":[],"ecmp_hashing_include_fields":{},"virtual_machine_interface_disable_policy":false,"qos_config_refs":[],"bridge_domain_refs":[],"perms2":{"owner_access":7,"global_access":0,"share":[]},"tag_refs":[]}};
      this.portMockData =
        [
          {
@@ -294,10 +297,188 @@
            }
          }
        ];
+       this.allVirtualnetworks = [
+           {
+               "virtual-network": {
+                 "parent_uuid": "bebe0474-6c75-492c-a5dc-fef977374915",
+                 "address_allocation_mode": "user-defined-subnet-only",
+                 "parent_type": "project",
+                 "route_target_list": {
+                   "route_target": []
+                 },
+                 "mac_learning_enabled": false,
+                 "pbb_etree_enable": false,
+                 "fq_name": [
+                   "default-domain",
+                   "admin",
+                   "test_refs"
+                 ],
+                 "uuid": "f353a90f-6e0e-4641-aff9-8d0fe74ca548",
+                 "name": "test_refs",
+                 "id_perms": {
+                   "enable": true,
+                   "uuid": {
+                     "uuid_mslong": 17533543658019506000,
+                     "uuid_lslong": 12680321325282929000
+                   },
+                   "created": "2018-01-22T11:34:12.024537",
+                   "description": null,
+                   "creator": null,
+                   "user_visible": true,
+                   "last_modified": "2018-01-22T11:40:20.698938",
+                   "permissions": {
+                     "owner": "admin",
+                     "owner_access": 7,
+                     "other_access": 7,
+                     "group": "admin",
+                     "group_access": 7
+                   }
+                 },
+                 "multi_policy_service_chains_enabled": false,
+                 "virtual_network_properties": {
+                   "allow_transit": false,
+                   "mirror_destination": false,
+                   "rpf": "enable"
+                 },
+                 "ecmp_hashing_include_fields": {},
+                 "network_policy_refs": [
+                   {
+                     "to": [
+                       "default-domain",
+                       "admin",
+                       "pol2"
+                     ],
+                     "attr": {
+                       "timer": null
+                     },
+                     "uuid": "9c5da855-f558-4a42-9d91-2b0bb2941b28"
+                   }
+                 ],
+                 "provider_properties": null,
+                 "perms2": {
+                   "owner": "bebe04746c75492ca5dcfef977374915",
+                   "owner_access": 7,
+                   "global_access": 0,
+                   "share": []
+                 },
+                 "display_name": "test_refs",
+                 "virtual_network_network_id": 5,
+                 "import_route_target_list": {
+                   "route_target": []
+                 },
+                 "is_shared": false,
+                 "router_external": false,
+                 "pbb_evpn_enable": false,
+                 "export_route_target_list": {
+                   "route_target": []
+                 },
+                 "flood_unknown_unicast": false,
+                 "layer2_control_word": false,
+                 "network_ipam_refs": [
+                   {
+                     "subnet": {
+                       "ipam_subnet": "11.11.11.0/24",
+                       "default_gateway": "11.11.11.1",
+                       "enable_dhcp": true,
+                       "dns_server_address": "11.11.11.2",
+                       "allocation_pools": "",
+                       "host_routes": "",
+                       "dhcp_option_list": "",
+                       "subnet_uuid": "26ca114a-5fb9-488a-892d-b76041530fff",
+                       "ipam": [
+                         "default-domain",
+                         "default-project",
+                         "default-network-ipam"
+                       ]
+                     }
+                   }
+                 ]
+               }
+             },
+             {
+               "virtual-network": {
+                 "virtual_network_properties": {
+                   "allow_transit": false,
+                   "mirror_destination": false,
+                   "rpf": "enable"
+                 },
+                 "ecmp_hashing_include_fields": {},
+                 "display_name": "vn_networks1",
+                 "is_shared": false,
+                 "address_allocation_mode": "user-defined-subnet-only",
+                 "export_route_target_list": {
+                   "route_target": []
+                 },
+                 "router_external": false,
+                 "name": "vn_networks1",
+                 "parent_uuid": "bebe0474-6c75-492c-a5dc-fef977374915",
+                 "parent_type": "project",
+                 "import_route_target_list": {
+                   "route_target": []
+                 },
+                 "route_target_list": {
+                   "route_target": []
+                 },
+                 "perms2": {
+                   "owner": "bebe04746c75492ca5dcfef977374915",
+                   "owner_access": 7,
+                   "global_access": 0,
+                   "share": []
+                 },
+                 "flood_unknown_unicast": false,
+                 "id_perms": {
+                   "enable": true,
+                   "uuid": {
+                     "uuid_mslong": 13604473922492647000,
+                     "uuid_lslong": 10643489204459215000
+                   },
+                   "created": "2018-01-22T11:17:20.684271",
+                   "description": null,
+                   "creator": null,
+                   "user_visible": true,
+                   "last_modified": "2018-01-22T11:17:20.684271",
+                   "permissions": {
+                     "owner": "admin",
+                     "owner_access": 7,
+                     "other_access": 7,
+                     "group": "admin",
+                     "group_access": 7
+                   }
+                 },
+                 "fq_name": [
+                   "default-domain",
+                   "admin",
+                   "vn_networks1"
+                 ],
+                 "uuid": "bcccccf3-f23d-4434-93b5-450a327f19fe",
+                 "multi_policy_service_chains_enabled": false,
+                 "virtual_network_network_id": 4,
+                 "network_ipam_refs": []
+               }
+             }
+           ];
+       this.securityGrpData = {
+               "security-groups": [
+                   {
+                     "href": "http://nodeg7:8082/security-group/0b550b5c-f240-4fa2-8840-aa571a84be00",
+                     "fq_name": [
+                       "default-domain",
+                       "admin",
+                       "default"
+                     ],
+                     "uuid": "0b550b5c-f240-4fa2-8840-aa571a84be00"
+                   }
+                 ]
+               };
+
        return {
            portDomainsData: portDomainsData,
            portPojectsData: portPojectsData,
            portUUIDListData: portUUIDListData,
-           portMockData: portMockData
+           portMockData: portMockData,
+           portModalMockCNInput: portModalMockCNInput,
+           portModalMockCNOutput: portModalMockCNOutput,
+           allVirtualnetworks: allVirtualnetworks,
+           securityGrpData: securityGrpData
        };
  });
