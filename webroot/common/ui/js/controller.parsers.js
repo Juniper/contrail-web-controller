2c55f070e4cd808c666ee51aecfe63392a73933a
Akhil <akhilf@juniper.net>
2018-02-13 14:56:59 +0530

    Add new page Floating IP pool with Permissions
    Currently we don't have option to set Permission for floating IP pool
    Hence added the new page for associating floating IP pool with permission
    under the selected vn
    
    Closes-Bug: #1732526
    Change-Id: Id928b93b21417babd1b0fb56317b8c2cc956287c

--- webroot/common/ui/js/controller.parsers.js
+++ webroot/common/ui/js/controller.parsers.js
@@ -3,7 +3,7 @@
  */
 
 define([
-    'underscore'
+    'lodash'
 ], function (_) {
     var CTParsers = function() {
         var self = this;
@@ -328,6 +328,72 @@ define([
            }
            return retArr;
         };
+        this.fipCfgPoolDataParser = function(self,response) {
+            var retArr = [];
+            var uuidTenant = [];
+            var gridEmptyArr = [];
+            var selectedProject = contrail.getCookie(cowc.COOKIE_PROJECT);
+            if(response[0] != null &&
+               'floating-ip-pools' in response[0] &&
+                response[0]['floating-ip-pools'] != null &&
+                response[0]['floating-ip-pools'].length > 0) {
+                var length = response[0]['floating-ip-pools'].length
+                for (var i = 0; i < length; i++) {
+                    retArr.push(response[0]['floating-ip-pools'][i]['floating-ip-pool']);
+                 //get all uuid teant
+                 if(response[0]['floating-ip-pools'][i]['floating-ip-pool'].perms2.share.length > 0){
+                    for(var j = 0; j < response[0]['floating-ip-pools'][i]['floating-ip-pool'].perms2.share.length; j++){
+                        if(response[0]['floating-ip-pools'][i]['floating-ip-pool'].perms2.share[j].tenant != null ||
+                                response[0]['floating-ip-pools'][i]['floating-ip-pool'].perms2.share[j].tenant != ''){
+                            uuidTenant.push(response[0]['floating-ip-pools'][i]['floating-ip-pool'].perms2.share[j].tenant);
+                        }
+                    }
+                  } 
+                }
+                //Call contrail ajax handler with prject 
+                var ajaxConfig = {};
+                var data;
+                ajaxConfig.type = "POST";
+                ajaxConfig.url = "/api/tenants/config/get-config-list"
+                ajaxConfig.data  = JSON.stringify({data: [{type: 'projects', obj_uuids:uuidTenant}]});
+                contrail.ajaxHandler(ajaxConfig,function () {
+                }, function (response) {
+                    for(var i = 0; i < retArr.length; i++){
+                        //Remove the duplicate elements
+                        var resultArr = [];
+                        var exits = {};
+                        var elm;
+                        var share = retArr[i].perms2.share;
+                        $.each(share, function(index, val) {
+                            elm = retArr[i].perms2.share[index].tenant;
+                            if(!exits[elm]){
+                                resultArr.push(retArr[i].perms2.share[index]);
+                                exits[elm] = true;
+                            }
+                        })
+                        for(var j = 0; j < resultArr.length; j++){
+                            for(var k = 0 ; k < response[0]['projects'].length; k++){
+                                if(response[0]['projects'][k].uuid === resultArr[j].tenant){
+                                    resultArr[j]["tenant_name"] = response[0]['projects'][k].fq_name[1];
+                                }
+                            }
+                        }
+                    }
+                    var projectFilterArray = [];
+                    _.each(retArr, function(item,index){
+                        if(item.fq_name[1] === selectedProject){
+                            projectFilterArray.push(item);
+                        }
+                    });
+                    self.contrailListModel.setData([]);
+                    self.contrailListModel.setData(projectFilterArray);
+                },
+                function (error) {
+                   console.log(error);
+               });
+            }
+            return gridEmptyArr;
+         };
 
         this.svcTemplateCfgDataParser = function(response) {
            var retArr = [];
