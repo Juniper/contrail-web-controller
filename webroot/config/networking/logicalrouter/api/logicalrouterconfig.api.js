35f14f14f4eecd2a9d651a2ece91fa57a31e4756
Petr Jediný <petr.jediny@gmail.com>
2018-02-21 13:04:30 +0100

    Disable port security on SNAT router_interface
    Change-Id: Ieacab0d8697dd6ca8945b22de2a50ca623ebd9b7
    Closes-Bug: #1750795

--- webroot/config/networking/logicalrouter/api/logicalrouterconfig.api.js
+++ webroot/config/networking/logicalrouter/api/logicalrouterconfig.api.js
@@ -264,6 +264,7 @@ function createLogicalRouterCB (dataObj, callback)
                 vmidata["virtual-machine-interface"]["display_name"] = uuid['hex'];
                 vmidata["virtual-machine-interface"]["uuid"] = uuid['hex'];
                 vmidata["virtual-machine-interface"]["name"] = uuid['hex'];
+                vmidata["virtual-machine-interface"]["port_security_enabled"] = false;
             }
             allDataArr.push({
                 request: request,
