9eaea6eebaf4fc33b43e701fbc9482c76ca65bec
Manoj Naik <mnaik@juniper.net>
2018-03-14 18:20:25 -0700

    Added more validation for the port range and port count.
    Added checks for non numeric, negatives, start lesser than end and mutually exclusive conditions.
    
    Change-Id: I29d264fdb1b65603575879f759906e64a551f086
    Closes-Bug: #1755407

--- webroot/config/infra/globalconfig/ui/js/models/portTranslationPoolsModel.js
+++ webroot/config/infra/globalconfig/ui/js/models/portTranslationPoolsModel.js
@@ -49,7 +49,11 @@ define([
                 },
                 'port_count': function(val, attr, data) {
                    if(data.port_range === '' && data.port_count !== ''){
-                       return '';
+                       if(!isNumber(data.port_count)){
+                           return 'Invalid Port Count';
+                       } else {
+                           return;
+                       }
                    }else if(data.port_range !== '' && data.port_count === ''){
                        return '';
                    }else if(data.port_range === '' && data.port_count === ''){
@@ -60,16 +64,24 @@ define([
                 },
                 'port_range': function(val, attr, data) {
                     if(data.port_count === '' && data.port_range !== ''){
-                        if(data.port_range.indexOf('-') !== -1){
+                        if(data.port_range.indexOf('-') !== -1 &&
+                                (data.port_range.match(/-/g) || []).length == 1){
                            var portRange = data.port_range.split('-');
                            if(!isNumber(portRange[0].trim()) && !isNumber(portRange[1].trim())){
                                return 'Invalid Port Range';
                            }
-                           if(!isNumber(portRange[0].trim())){
+                           if(!isNumber(portRange[0].trim()) ||
+                                   portRange[0] < 0 ||
+                                   portRange[0] > 65535){
                               return 'Invalid Start Port';
                            }
-                           if(!isNumber(portRange[1].trim())){
+                           if(!isNumber(portRange[1].trim()) ||
+                                   portRange[1] < 0 ||
+                                   portRange[1] > 65535){
                                return 'Invalid End Port';
+                            }
+                           if(portRange[0] >= portRange[1]){
+                               return 'Invalid Port Range';
                             }
                         }else{
                            return 'Invalid Port Range';
