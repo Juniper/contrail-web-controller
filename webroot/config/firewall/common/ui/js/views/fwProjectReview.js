cf0185526f7d66ee09fa00a445cc8388be98b866
Suresh Akula <surakula@juniper.net>
2018-03-14 21:47:38 -0700

    Security policy draft changes - phase1   1. For global scope, added a new tab ‘Security Policy Options’ under Configure—>Infrastructure—>Global Config.   2. For project scope, added a new tab ‘Security Policy’ in Configure—>Infrastructure—>Project Settings.   3. Added Firewall Rules tab to landing page.   4. Added Commit/Draft toggle to list committed/drafted security resources in both global and project views.   5. Added Review button place holder
    Fixed Global Config tabs Layout, organized based on global Config categories
    
    Added new resource to Configure --> Project Settings --> Quotas
       a. Loadbalancers
       b. Loadbalancers Listeners
    
    Fixed UT
    
    Change-Id: I79d0d3afe91232f45ebb244c29f10a8f71f5b15c
    Partial-bug: #1746328
    Partial-bug: #1751019

---
+++ webroot/config/firewall/common/ui/js/views/fwProjectReview.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view',
+    "contrail-config-model",
+    'knockback',
+    'config/firewall/common/ui/js/views/fwReviewConfigs'
+], function (_, ContrailView, ContrailConfigModel, Knockback, FWReviewConfigs) {
+    var prefixId = ctwc.FW_PROJECT_REVIEW_PREFIX_ID;
+    var modalId = 'review-' + prefixId;
+    var formId = '#' + modalId + '-form';
+    var self, reviewConfigs = new FWReviewConfigs();
+    
+    var fwProjectReview = ContrailView.extend({
+        renderReviewFW: function (options) {
+            var editTemplate =
+            contrail.getTemplate4Id(ctwl.TMPL_CORE_GENERIC_VIEW),
+            editLayout = editTemplate({prefixId: prefixId, modalId: modalId}),
+            self = this;
+            
+            cowu.createModal({
+            		'modalId': modalId,
+                'className': 'modal-980',
+                'title': options['title'],
+                'body': editLayout,
+            	    'onCommit': function () {
+                    self.configReviewFW(options);
+            }, 'onRevert': function () {
+                Knockback.release(self.model, document.getElementById(modalId));
+                kbValidation.unbind(self);
+                $("#" + modalId).modal('hide');
+            }, 'onCancel': function () {
+                Knockback.release(self.model, document.getElementById(modalId));
+                kbValidation.unbind(self);
+                $("#" + modalId).modal('hide');
+            }});
+            self.fwRenderReview4Config();
+        },
+
+       configReviewFW : function(options) {
+           Knockback.release(self.model, document.getElementById(modalId));
+           kbValidation.unbind(self);
+           $("#" + modalId).modal('hide');
+        },
+
+        fwRenderReview4Config : function() {
+            this.renderView4Config(
+            		$("#" + modalId).find("#core-view"),
+               new ContrailConfigModel(),
+                reviewConfigs.viewConfig(prefixId),
+                "", null, null,
+                function () {
+                   /* self.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID,
+                                             false);
+                    Knockback.applyBindings(self.model,
+                        document.getElementById(modalId));
+                    kbValidation.bind(self,
+                       {collection:
+                           self.model.model().attributes.dscp_entries_fc_pair});
+                    kbValidation.bind(self,
+                        {collection:
+                            self.model.model().attributes.
+                                vlan_priority_entries_fc_pair});
+                    kbValidation.bind(self,
+                        {collection:
+                            self.model.model().attributes.
+                            mpls_exp_entries_fc_pair});
+                    //permissions
+                    ctwu.bindPermissionsValidation(self);*/
+                }, null, null
+            );
+        }
+    });
+
+    return fwProjectReview;
+});
+
+
