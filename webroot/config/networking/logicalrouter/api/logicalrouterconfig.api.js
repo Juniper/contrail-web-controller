f3a2aab2b321f8596b80c6a623c995488768445a
Manoj Naik <mnaik@juniper.net>
2018-03-22 12:43:37 -0700

    Revert "Disable port security on SNAT router_interface"
    Reverting this change as per the discussion.
    
    Related-Bug: #1750795
    
    This reverts commit 48b32dd8de34514f189f2cdbe8ed491563d5fc2b.
    
    Change-Id: Icf64da0f5f6b705f9b79749d20773f2509e8971b

--- webroot/config/networking/logicalrouter/api/logicalrouterconfig.api.js
+++ webroot/config/networking/logicalrouter/api/logicalrouterconfig.api.js
@@ -250,7 +250,6 @@ function createLogicalRouter(request, response, appData)
                 vmidata["virtual-machine-interface"]["display_name"] = uuid['hex'];
                 vmidata["virtual-machine-interface"]["uuid"] = uuid['hex'];
                 vmidata["virtual-machine-interface"]["name"] = uuid['hex'];
-                vmidata["virtual-machine-interface"]["port_security_enabled"] = false;
             }
             allDataArr.push({
                 request: request,
@@ -392,7 +391,6 @@ function readLogicalRouterToUpdate(error, logicalRouterURL, orginalDataFromUI, l
                     vmidata["virtual-machine-interface"]["display_name"] = uuid['hex'];
                     vmidata["virtual-machine-interface"]["uuid"] = uuid['hex'];
                     vmidata["virtual-machine-interface"]["name"] = uuid['hex'];
-                    vmidata["virtual-machine-interface"]["port_security_enabled"] = false;
                 }
                 allDataArr.push({
                     request: request,
