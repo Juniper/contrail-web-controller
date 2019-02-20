6a7f8b2fed330b77ed0b08dd94ff0071b383f437
ZheyangSong <jerome_soung@hotmail.com>
2019-02-20 11:58:16 -0800

    Remove 'import_route_target_list' to Allow VN Creation
    When 'import_route_target_list' is present in request payload,
    VNC api rejects the VN creation request.
    
    close-jira-bug: JCB-183248
    Change-Id: I81b133216504d79acd4130981f98dbe315f73886

--- webroot/config/infra/vrouters/ui/js/models/vRouterCfgModel.js
+++ webroot/config/infra/vrouters/ui/js/models/vRouterCfgModel.js
@@ -14,7 +14,7 @@ define([
         constructor: function () {
             ContrailConfigModel.apply(this,arguments);
             if (this.virtual_router_type() == '' ||
-                this.virtual_router_type() == [] ||
+                !this.virtual_router_type().length ||
                 this.virtual_router_type() == null) {
                 this.virtual_router_type('hypervisor');
             }
@@ -212,7 +212,7 @@ define([
                 if (newvRouterCfgData['display_name'] == '') {
                     newvRouterCfgData['display_name'] = newvRouterCfgData['name'];
                 }
-                if (newvRouterCfgData['fq_name'] == [] ||
+                if (!newvRouterCfgData['fq_name'].length ||
                     newvRouterCfgData['fq_name'] == null) {
                     newvRouterCfgData['fq_name'] = [];
                     newvRouterCfgData['fq_name'][0] = 'default-global-system-config';
